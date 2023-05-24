c1e578984fe29db6f82f6cca652d54a7238812f0
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2023-05-24 11:16:12 +0200

    creating multithreaded matchms functions and process.

--- scripts/MAIN.py
+++ scripts/MAIN.py
@@ -1,18 +1,41 @@
 from msp_utilities import *
+from matchms_treatment import *
+import sys
 import os
 
-input_path = r"..\INPUT"
-output_path = r"..\OUTPUT"
+if __name__ == "__main__":
+    input_path = r"..\INPUT"
+    output_path = r"..\OUTPUT"
+
+    # STEP 1: convert files to msp if needed
+    convert_to_msp(input_path)
+
+    # STEP 2: split spectrums into a list AND matchms clean
+    msp_dir = os.path.join(input_path, "MSP")
+
+    # Check if there is msp file to process
+    msp_to_process = False
+    for files in os.listdir(msp_dir):
+        if files.endswith(".msp"):
+            msp_to_process = True
+            break
+
+    # If there is no msp to process: stop python execution
+    if msp_to_process == False:
+        sys.exit("There is no msp file to process into \"./INPUT/MSP\". Exiting code !")
+
+    # Creating list of spectrums
+    for files in os.listdir(msp_dir):
+        if files.endswith(".msp"):
+            msp_path = os.path.join(msp_dir, files)
+            file_name = os.path.basename(msp_path.replace(".msp", ""))
+            spectrum_list = split_spectrums(msp_path)
+
+            matchms_treatment(spectrum_list)
+            # TO BE CONTINUED ... to sth with clean_msp_list
+
+            # Write matchms clean msp into new msp file
+            with open(os.path.join(r"..\OUTPUT\CLEAN_MSP)",file_name+"_clean"+".msp"), "w", encoding="UTF-8") as clean:
+                clean.write("\n\n".join(clean_msp_list))
 
 
-# STEP 1: convert files to msp if needed
-convert_to_msp(input_path)
-
-# STEP 2: split spectrums into a list AND matchms clean
-msp_dir = os.path.join(input_path,"MSP")
-for files in os.listdir(msp_dir):
-    if files.endswith(".msp"):
-        msp_path = os.path.join(msp_dir,files)
-        spectrums = split_spectrums(msp_path)
-
-        # TO BE CONTINUED ...
---
+++ scripts/matchms_treatment.py
@@ -0,0 +1,49 @@
+from matchms.logging_functions import set_matchms_logger_level, add_logging_to_file
+from matchms.importing import load_from_msp
+from matchms.exporting import save_as_msp
+from tqdm.notebook import tqdm as tqdm
+import matchms.filtering as msfilters
+import matchms.metadata_utils
+import matchms.Fragments
+import matchms.Metadata
+import matchms.hashing
+
+import concurrent.futures
+
+clean_msp_list = []
+
+def multithreaded_matchms(spectrum):
+    # apply_metadata_filters
+    spectrum = msfilters.default_filters(spectrum)
+    spectrum = msfilters.derive_adduct_from_name(spectrum)
+    spectrum = msfilters.add_parent_mass(spectrum, estimate_from_adduct=True)
+    spectrum = msfilters.add_retention.add_retention_time(spectrum)
+    spectrum = msfilters.set_ionmode_na_when_missing(spectrum)
+    spectrum = matchms.metadata_utils.clean_adduct(spectrum)
+    spectrum = msfilters.repair_inchi_inchikey_smiles(spectrum)
+    spectrum = msfilters.derive_inchi_from_smiles(spectrum)
+    spectrum = msfilters.derive_smiles_from_inchi(spectrum)
+    spectrum = msfilters.derive_inchikey_from_inchi(spectrum)
+    spectrum = msfilters.harmonize_undefined_smiles(spectrum)
+    spectrum = msfilters.harmonize_undefined_inchi(spectrum)
+    spectrum = msfilters.harmonize_undefined_inchikey(spectrum)
+    spectrum = msfilters.add_precursor_mz(spectrum)
+
+    # normalize_and_filter_peaks
+    spectrum = msfilters.normalize_intensities(spectrum)
+    spectrum = msfilters.select_by_relative_intensity(spectrum, 0.01, 1)
+    spectrum = msfilters.select_by_mz(spectrum, mz_from=50, mz_to=2000.0)
+    spectrum = msfilters.reduce_to_number_of_peaks(spectrum, n_max=500)
+    spectrum = msfilters.require_minimum_number_of_peaks(spectrum, n_required=3)
+
+    return spectrum
+
+
+def app_msp(spectrum):
+    clean_msp_list.append(spectrum)
+
+def matchms_treatment(spectrum_list):
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        future_proc = {executor.submit(multithreaded_matchms, spectrum): spectrum for spectrum in spectrum_list}
+        for future in concurrent.futures.as_completed(future_proc):
+            app_msp(future.result())
