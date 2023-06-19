b0343f230ea792e9c67c80af5048616bb188630b
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2023-06-19 14:41:14 +0200

    adding script to make tests

---
+++ scripts/extract_n_random_spectrums.py
@@ -0,0 +1,26 @@
+import os
+import re
+import  random
+
+paths = "../../ORIGINALS_msp_DB/"
+
+n = 100
+
+for dir in os.listdir(paths):
+    dir_path = os.path.join(paths,dir)
+    if os.path.isdir(dir_path):
+        for files in os.listdir(dir_path):
+            msp_path = os.path.join(dir_path,files)
+            if files.endswith(".msp"):
+                with open(msp_path,"r",encoding="UTF-8") as msp_buffer:
+                    spectrum_list = msp_buffer.read().split("\n\n")
+
+                random_n_list = random.sample(spectrum_list, n)
+
+                # final_random_n_list = [re.sub("charge: -\n","CHARGE: -1\n",spectrum, flags=re.I) for spectrum in random_n_list]
+
+                with open(os.path.join("../../SOUS_DB","SOUS_"+files.replace(".msp","")+".msp"),"w",encoding="UTF-8") as msp_buffer:
+                    msp_buffer.write("\n\n".join(random_n_list))
+
+
+
