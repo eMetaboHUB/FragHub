cac7eb55b3f0a5c9bb6cfd387b3424d1d94b81a8
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2023-06-20 14:02:15 +0200

    creating function to delete duplicates spectrums

--- scripts/MAIN.py
+++ scripts/MAIN.py
@@ -63,6 +63,10 @@ if __name__ == "__main__":
 
     POS, NEG = split_pos_neg(CONCATENATE_LIST)
 
+    # STEP 5: Remove duplicates spectrum when same peak_list for the same inchikey.
+    POS, NEG = remove_duplicatas(POS, NEG)
+
+
     POS_FULL = re.sub("\n{2,}","\n\n\n","\n\n".join(POS))
     NEG_FULL = re.sub("\n{2,}","\n\n\n","\n\n".join(NEG))
 
--- scripts/msp_utilities.py
+++ scripts/msp_utilities.py
@@ -521,6 +521,60 @@ def msp_to_csv(clean_msp_path):
     NEG_df = pd.DataFrame.from_dict(dictionary)
     NEG_df.to_csv("../OUTPUT/CSV/FINAL_NEG/NEG_clean.csv", sep=";", encoding="UTF-8", index=False)
 
+def find_indices(l, value):
+    duplicatas_index = [index for index, item in enumerate(l) if item == value]
+    if duplicatas_index != None:
+        if len(duplicatas_index) >= 2:
+            duplicatas_index = duplicatas_index[1:]
+            return duplicatas_index
+        else:
+            return []
+    else:
+        return []
+
+def remove_duplicatas(POS, NEG):
+    # POS
+    POS_list = []
+    POS_index_to_delete = []
+    for spectrum in POS: # Générer une liste donnée réduite à inchikey + peak_list
+        POS_list.append(str({"INCHIKEY": re.search("INCHIKEY: (.*)\n",spectrum).group(1), "PEAK_LIST": re.search("(NUM PEAKS: [0-9]*)\n([\s\S]*)",spectrum).group(2)}))
+
+    for current_dict in POS_list:
+        POS_index_to_delete.extend(find_indices(POS_list,current_dict))
+
+    POS_FILTERED = []
+    compteur = 0
+    for spectrum in POS:
+        if compteur not in POS_index_to_delete:
+            POS_FILTERED.append(spectrum)
+        compteur += 1
+
+    # NEG
+    NEG_list = []
+    NEG_index_to_delete = []
+    for spectrum in NEG:  # Générer une liste donnée réduite à inchikey + peak_list
+        NEG_list.append({"INCHIKEY": re.search("INCHIKEY: (.*)\n", spectrum).group(1),
+                         "PEAK_LIST": re.search("(NUM PEAKS: [0-9]*)\n([\s\S]*)", spectrum).group(2)})
+
+    for current_dict in NEG_list:
+        NEG_index_to_delete.extend(find_indices(NEG_list, current_dict))
+
+    NEG_FILTERED = []
+    compteur = 0
+    for spectrum in NEG:
+        if compteur not in NEG_index_to_delete:
+            NEG_FILTERED.append(spectrum)
+        compteur += 1
+
+    return POS_FILTERED,NEG_FILTERED
+
+
+
+
+
+
+
+
 
 
 
