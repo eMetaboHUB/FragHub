3f42f48ef269e7e0bec8e3a44af14e52153e90fe
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2023-06-22 10:35:26 +0200

    moving duplicatas functions to duplicatas_remover.py

---
+++ scripts/duplicatas_remover.py
@@ -0,0 +1,47 @@
+import re
+
+def find_indices(l, value):
+    duplicatas_index = [index for index, item in enumerate(l) if item == value]
+    if duplicatas_index != None:
+        if len(duplicatas_index) >= 2:
+            duplicatas_index = duplicatas_index[1:]
+            return duplicatas_index
+        else:
+            return []
+    else:
+        return []
+def remove_duplicatas(POS, NEG):
+    # POS
+    POS_list = []
+    POS_index_to_delete = []
+    for spectrum in POS: # Générer une liste donnée réduite à inchikey + peak_list
+        POS_list.append(str({"INCHIKEY": re.search("INCHIKEY: (.*)\n",spectrum).group(1), "PEAK_LIST": re.search("(NUM PEAKS: [0-9]*)\n([\s\S]*)",spectrum).group(2)}))
+
+    for current_dict in POS_list:
+        POS_index_to_delete.extend(find_indices(POS_list,current_dict))
+
+    POS_FILTERED = []
+    compteur = 0
+    for spectrum in POS:
+        if compteur not in POS_index_to_delete:
+            POS_FILTERED.append(spectrum)
+        compteur += 1
+
+    # NEG
+    NEG_list = []
+    NEG_index_to_delete = []
+    for spectrum in NEG:  # Générer une liste donnée réduite à inchikey + peak_list
+        NEG_list.append({"INCHIKEY": re.search("INCHIKEY: (.*)\n", spectrum).group(1),
+                         "PEAK_LIST": re.search("(NUM PEAKS: [0-9]*)\n([\s\S]*)", spectrum).group(2)})
+
+    for current_dict in NEG_list:
+        NEG_index_to_delete.extend(find_indices(NEG_list, current_dict))
+
+    NEG_FILTERED = []
+    compteur = 0
+    for spectrum in NEG:
+        if compteur not in NEG_index_to_delete:
+            NEG_FILTERED.append(spectrum)
+        compteur += 1
+
+    return POS_FILTERED,NEG_FILTERED
