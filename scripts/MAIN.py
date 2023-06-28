39848e2535ae3d0317d8c24fc9895e561600c6b5
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2023-06-28 16:35:57 +0200

    updating lrsv duplicatas filter

--- scripts/MAIN.py
+++ scripts/MAIN.py
@@ -77,13 +77,13 @@ if __name__ == "__main__":
 
 
     # STEP 6: Remove duplicates spectrum when same peak_list for the same inchikey.
-    print("-- REMOVING DUPLICATAS --")
-    time.sleep(0.01)
-    POS_LC,POS_LC_df,POS_GC,POS_GC_df,NEG_LC,NEG_LC_df,NEG_GC,NEG_GC_df = remove_duplicatas_public(POS_LC,POS_GC,NEG_LC,NEG_GC)
-
-    # print("REMOVING DUPLICATAS")
+    # print("-- REMOVING DUPLICATAS --")
     # time.sleep(0.01)
-    # POS_LC_FILTERED,POS_GC_FILTERED,NEG_LC_FILTERED,NEG_GC_FILTERED = remove_duplicatas_lrsv(POS_LC,POS_GC,NEG_LC,NEG_GC)
+    # POS_LC,POS_LC_df,POS_GC,POS_GC_df,NEG_LC,NEG_LC_df,NEG_GC,NEG_GC_df = remove_duplicatas_public(POS_LC,POS_GC,NEG_LC,NEG_GC)
+
+    print("REMOVING DUPLICATAS")
+    time.sleep(0.01)
+    POS_LC_FILTERED,POS_GC_FILTERED,NEG_LC_FILTERED,NEG_GC_FILTERED = remove_duplicatas_lrsv(POS_LC,POS_GC,NEG_LC,NEG_GC)
 
     print("-- WRITING MSP --")
 
