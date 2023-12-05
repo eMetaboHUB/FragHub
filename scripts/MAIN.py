71062c5b92655dea89a72de77cfa35818323b9ab
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2023-12-05 10:56:37 +0100

    renaming fragbank to fraghub

--- scripts/MAIN.py
+++ scripts/MAIN.py
@@ -19,59 +19,59 @@ if __name__ == "__main__":
     input_path = r"..\INPUT"
     output_path = r"..\OUTPUT"
 
-    # # STEP 1: convert files to msp if needed (Multithreaded)
-    # FINAL_JSON,FINAL_XML = convert_to_msp(input_path)
-    #
-    # if FINAL_JSON != [] :
-    #     with open(os.path.join("../INPUT/MSP/JSON_converted"+".msp"), "a", encoding="UTF-8") as temp:
-    #         temp.write("\n\n".join(FINAL_JSON))
-    #
-    # if FINAL_XML != []:
-    #     with open(os.path.join("../INPUT/MSP/XML_converted"+".msp"), "a", encoding="UTF-8") as temp:
-    #         temp.write("\n\n".join(FINAL_XML))
-    #
-    # del FINAL_JSON
-    # del FINAL_XML
-    #
-    # # STEP 2: split spectrums into a list AND matchms clean
-    # msp_dir = os.path.join(input_path, "MSP")
-    #
-    # # Check if there is msp file to process
-    # msp_to_process = False
-    # for files in os.listdir(msp_dir):
-    #     if files.endswith(".msp"):
-    #         msp_to_process = True
-    #         break
-    #
-    # # If there is no msp to process: stop python execution
-    # if msp_to_process == False:
-    #     sys.exit("There is no msp file to process into \"./INPUT/MSP\". Exiting code !")
-    #
-    # # generating FRAGBANKID
-    # print("-- GENERATING FragBank UNIQUE ID --")
-    # time.sleep(0.01)
-    # unique_id_generator()
-    #
-    # # Creating list of spectrums
-    # for files in os.listdir(msp_dir):
-    #     if files.endswith(".msp"):
-    #         msp_path = os.path.join(msp_dir, files)
-    #         file_name = os.path.basename(msp_path.replace(".msp", ""))
-    #
-    #         correct_uncomplete_charge(msp_path)
-    #
-    #         # STEP 3: Execute matchms (Multithreaded)
-    #         print("-- MATCHMS PROCESSING ON: ",file_name," --")
-    #         spectrum_list = list(load_from_msp(msp_path))
-    #         results = matchms_processing(spectrum_list,file_name)
-    #
-    #         del spectrum_list
-    #
-    #         # Write matchms clean msp into new msp file
-    #         with open(os.path.join(r"../OUTPUT/MSP", file_name + "_clean" + ".msp"), "w", encoding="UTF-8") as clean:
-    #             clean.write("\n\n".join(results))
-    #
-    #         del results
+    # STEP 1: convert files to msp if needed (Multithreaded)
+    FINAL_JSON,FINAL_XML = convert_to_msp(input_path)
+
+    if FINAL_JSON != [] :
+        with open(os.path.join("../INPUT/MSP/JSON_converted"+".msp"), "a", encoding="UTF-8") as temp:
+            temp.write("\n\n".join(FINAL_JSON))
+
+    if FINAL_XML != []:
+        with open(os.path.join("../INPUT/MSP/XML_converted"+".msp"), "a", encoding="UTF-8") as temp:
+            temp.write("\n\n".join(FINAL_XML))
+
+    del FINAL_JSON
+    del FINAL_XML
+
+    # STEP 2: split spectrums into a list AND matchms clean
+    msp_dir = os.path.join(input_path, "MSP")
+
+    # Check if there is msp file to process
+    msp_to_process = False
+    for files in os.listdir(msp_dir):
+        if files.endswith(".msp"):
+            msp_to_process = True
+            break
+
+    # If there is no msp to process: stop python execution
+    if msp_to_process == False:
+        sys.exit("There is no msp file to process into \"./INPUT/MSP\". Exiting code !")
+
+    # generating FRAGHUBID
+    print("-- GENERATING FragHub UNIQUE ID --")
+    time.sleep(0.01)
+    unique_id_generator()
+
+    # Creating list of spectrums
+    for files in os.listdir(msp_dir):
+        if files.endswith(".msp"):
+            msp_path = os.path.join(msp_dir, files)
+            file_name = os.path.basename(msp_path.replace(".msp", ""))
+
+            correct_uncomplete_charge(msp_path)
+
+            # STEP 3: Execute matchms (Multithreaded)
+            print("-- MATCHMS PROCESSING ON: ",file_name," --")
+            spectrum_list = list(load_from_msp(msp_path))
+            results = matchms_processing(spectrum_list,file_name)
+
+            del spectrum_list
+
+            # Write matchms clean msp into new msp file
+            with open(os.path.join(r"../OUTPUT/MSP", file_name + "_clean" + ".msp"), "w", encoding="UTF-8") as clean:
+                clean.write("\n\n".join(results))
+
+            del results
 
     # # STEP 4: (All msp files were cleaned)
     clean_msp_path = os.path.join(output_path,"MSP")
