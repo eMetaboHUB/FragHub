a5e1c370580d3314a64b57b7d98544a39124e273
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2023-12-07 09:52:22 +0100

    Add docstrings for functions in msp_utilities.py
    Docstrings have been added for all functions in the `msp_utilities.py` script. These provide a brief summary of the function's purpose, describe the parameters, return type, and give a more detailed explanation of what each function does. This enhances code readability and is friendly for developers who contribute to or use this script.

--- scripts/msp_utilities.py
+++ scripts/msp_utilities.py
@@ -11,6 +11,14 @@ import os
 
 
 def concatenate_clean_msp(clean_msp_path):
+    """
+    Concatenates all spectrums of all cleaned files in the given directory.
+
+    :param clean_msp_path: Path to the directory containing the cleaned files.
+    :type clean_msp_path: str
+    :return: List of concatenated spectrums.
+    :rtype: list[str]
+    """
     CONCATENATE_LIST = []
     # append all spectrums of all cleaned files into CONCATENATE_LIST
     for files in os.listdir(clean_msp_path):
@@ -23,6 +31,12 @@ def concatenate_clean_msp(clean_msp_path):
     return CONCATENATE_LIST
 
 def msp_to_csv(CONCATENATE_LIST):
+    """
+    Converts a list of spectra in MSP format to a DataFrame in CSV format.
+
+    :param CONCATENATE_LIST: A list of spectra in MSP format.
+    :return: A DataFrame in CSV format.
+    """
     dictionary = {}
     CONCATENATE_DF = pd.DataFrame()
     first = True
@@ -55,6 +69,12 @@ def msp_to_csv(CONCATENATE_LIST):
     return CONCATENATE_DF
 
 def correct_uncomplete_charge(msp_path):
+    """
+    Corrects the incomplete charge in the MSP file.
+
+    :param msp_path: The path to the MSP file.
+    :return: None
+    """
     with open(msp_path, "r", encoding="UTF-8") as msp_buffer:
         content = msp_buffer.read()
 
@@ -64,12 +84,30 @@ def correct_uncomplete_charge(msp_path):
         msp_buffer.write(content)
 
 def update_names(group):
+    """
+    :param group: a pandas DataFrame representing a group of data
+    :return: the group with updated names
+
+    This method updates the 'NAME' column in the given group DataFrame. It checks if any value in the 'NAME' column is not null. If there is at least one non-null value, it selects the first
+    * non-null value and assigns it to all rows in the 'NAME' column. Finally, it returns the updated group DataFrame.
+    """
     if group['NAME'].notnull().any():
         non_empty_name = group.loc[group['NAME'].notnull(), 'NAME'].iloc[0]
         group['NAME'] = non_empty_name
     return group
 
 def names_completion(CONCATENATE_DF):
+    """
+    :param CONCATENATE_DF: The input DataFrame containing the 'NAME' column.
+    :return: The updated DataFrame with the empty values in the 'NAME' column replaced by 'None'.
+
+    This method takes a DataFrame as input and performs the following operations:
+    1. Replaces the string 'None' with an empty string in the 'NAME' column of the input DataFrame.
+    2. Groups the DataFrame by the 'INCHI' column.
+    3. Applies the 'update_names' function to each group of the grouped DataFrame.
+    4. Replaces the empty values in the 'NAME' column of the updated DataFrame with the string 'None'.
+    5. Returns the updated DataFrame.
+    """
     CONCATENATE_DF['NAME'] = CONCATENATE_DF['NAME'].replace('None', '')
 
     grouped = CONCATENATE_DF.groupby('INCHI')
@@ -81,6 +119,12 @@ def names_completion(CONCATENATE_DF):
     return updated_df
 
 def inchi_smiles_completion(CONCATENATE_LIST):
+    """
+    Update missing INCHI and SMILES values in the given list of spectra using the corresponding INCHIKEY values.
+
+    :param CONCATENATE_LIST: A list of spectra to be processed.
+    :return: The updated list of spectra with missing INCHI and SMILES values completed.
+    """
     inchikey_inchi = {}
     inchikey_smiles = {}
 
@@ -127,6 +171,14 @@ def inchi_smiles_completion(CONCATENATE_LIST):
     return updated_spetcrum_list
 
 def remove_no_smiles_inchi(CONCATENATE_LIST):
+    """
+    Remove spectra from CONCATENATE_LIST that do not have SMILES or INCHI information.
+
+    :param CONCATENATE_LIST: A list of spectra
+    :type CONCATENATE_LIST: list
+    :return: List of spectra with SMILES or INCHI information
+    :rtype: list
+    """
     CONCATENATE_LIST_temp = []
     for spectrums in tqdm(CONCATENATE_LIST, total=len(CONCATENATE_LIST), unit=" spectrums", colour="green", desc="\t  processing"):
         if not (re.search("SMILES: None\n",spectrums) or re.search("INCHI: None\n",spectrums)):
@@ -135,6 +187,11 @@ def remove_no_smiles_inchi(CONCATENATE_LIST):
     return CONCATENATE_LIST_temp
 
 def unique_id_generator():
+    """
+    Generate unique IDs for files in the INPUT directory.
+
+    :return: None
+    """
     INPUT_path = "../INPUT/MSP"
     for files in os.listdir(INPUT_path):
         if files.endswith(".msp"):
@@ -156,6 +213,13 @@ def unique_id_generator():
                     buffer.write("\n\n\n".join(temp_list))
 
 def apply_transformations(row):
+    """
+    Apply transformations to the given row.
+
+    :param row: A dictionary representing a row of data.
+    :return: The transformed row with additional properties.
+
+    """
     if not pd.isna(row['INCHI']):
         mol = Chem.MolFromInchi(row['INCHI'])
         if mol is not None:
@@ -171,6 +235,12 @@ def apply_transformations(row):
     return row
 
 def mols_derivator(CONCATENATE_DF):
+    """
+    Derives molecular properties and removes rows with missing InChI, SMILES, or InChIKey values.
+
+    :param CONCATENATE_DF: A pandas DataFrame containing molecular data.
+    :return: A modified pandas DataFrame with derived properties and missing rows removed.
+    """
     total_rows = len(CONCATENATE_DF)
     t = tqdm(total=total_rows, unit=" rows", colour="green", desc="\t  generating")
 
@@ -193,6 +263,13 @@ def mols_derivator(CONCATENATE_DF):
     return CONCATENATE_DF
 
 def mass_calculation(row):
+    """
+    Calculates the exact and average mass of a chemical compound represented by its SMILES string.
+
+    :param row: a Pandas DataFrame row containing a 'SMILES' column
+    :return: the modified row with additional 'EXACTMASS' and 'AVERAGEMASS' columns
+
+    """
     if not pd.isna(row['SMILES']):
         SMILES = str(row['SMILES'])
         if SMILES != "None":
@@ -215,6 +292,12 @@ def mass_calculation(row):
     return row
 
 def mass_calculator(CONCATENATE_DF):
+    """
+    Calculate mass for each row in the CONCATENATE_DF.
+
+    :param CONCATENATE_DF: A pandas DataFrame containing the data.
+    :return: The modified CONCATENATE_DF with mass calculated for each row.
+    """
     total_rows = len(CONCATENATE_DF)
     t = tqdm(total=total_rows, unit=" rows", colour="green", desc="\t  processing")
 
