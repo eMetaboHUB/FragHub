778d193726b825d66ef4ade5aa2df8f9c49d626a
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2023-12-15 17:07:05 +0100

    Refactor and simplify peak dataframe functions' documentation
    The commit improves the clarity of the function documentation in Filters_NEW.py. Changes include rewording and simplification of the function descriptions to enhance readability and understanding. Additionally, it replaces the use of None with an empty DataFrame for unmet conditions to allow for uniform return types.

--- scripts/Filters_NEW.py
+++ scripts/Filters_NEW.py
@@ -2,9 +2,11 @@ import pandas as pd
 
 def remove_peak_above_precursormz(peak_dataframe, precursormz):
     """
-    :param peak_dataframe: The dataframe containing peak information.
-    :param precursormz: The precursor m/z value used as a threshold for filtering peaks.
-    :return: The filtered dataframe with peaks having m/z values below the provided precursor m/z + 5.0 Da.
+    Removes peaks from `peak_dataframe` with mz values greater than `precursormz` + 5.
+
+    :param peak_dataframe: A DataFrame containing peak data.
+    :param precursormz: The precursor m/z value.
+    :return: A DataFrame with peaks removed.
     """
     peak_dataframe = peak_dataframe[peak_dataframe['mz'] < float(precursormz) + 5.0]  # Removing peaks with mz > precursormz + 5 Da
 
@@ -12,11 +14,10 @@ def remove_peak_above_precursormz(peak_dataframe, precursormz):
 
 def reduce_peak_list(peak_dataframe):
     """
-    :param peak_dataframe: A DataFrame containing peak data.
-    :return: A reduced DataFrame containing the top 500 peaks based on intensity, sorted by mz.
+    Reduce the peak list to a maximum of 500 peaks.
 
-    The reduce_peak_list method takes a peak_dataframe as input and returns a reduced DataFrame containing the top 500 peaks based on intensity. The method first sorts the peak_dataframe
-    * in descending order based on intensity and retains the top 500 rows. It then sorts the resulting DataFrame by mz.
+    :param peak_dataframe: The dataframe containing the peak data.
+    :return: The reduced peak dataframe.
     """
     if len(peak_dataframe) > 500:
         # Triez le DataFrame par intensité en ordre décroissant et gardez les 500 premières lignes
@@ -28,39 +29,37 @@ def reduce_peak_list(peak_dataframe):
 
 def check_minimum_peak_requiered(peak_dataframe):
     """
-    Checks if the given peak_dataframe has the minimum required number of peaks.
+    Check if the given peak dataframe meets the minimum requirement.
 
-    :param peak_dataframe: A dataframe containing peak information.
-    :type peak_dataframe: pandas.DataFrame
-
-    :return: The peak_dataframe if it has at least 3 peaks, None otherwise.
-    :rtype: pandas.DataFrame or None
+    :param peak_dataframe: A pandas dataframe representing the peak data.
+    :return: A pandas dataframe. If the length of the peak dataframe is less than 3, an empty dataframe is returned. Otherwise, the peak dataframe is returned as is.
     """
     if len(peak_dataframe) < 3:
-        return None
+        return pd.DataFrame()
     else:
         return peak_dataframe
 
 def normalize_intensity(peak_dataframe):
     """
-    Method to normalize the intensity values in a peak dataframe.
+    Normalize the intensity values in the given peak_dataframe.
 
-    :param peak_dataframe: pandas DataFrame containing peak data with 'intensity' column
-    :return: pandas DataFrame with normalized intensity values
-
-    This method uses the formula (value - min) / (max - min) to normalize the intensity values
-    in the peak dataframe. The 'intensity' column is modified in-place.
+    :param peak_dataframe: The dataframe containing peak information.
+    :type peak_dataframe: pandas.DataFrame
+    :return: The normalized peak_dataframe.
+    :rtype: pandas.DataFrame
     """
-    peak_dataframe['intensity'] = peak_dataframe['intensity'] = peak_dataframe['intensity']/peak_dataframe['intensity'].max()
+    peak_dataframe.loc[:, 'intensity'] = peak_dataframe['intensity']/peak_dataframe['intensity'].max()
 
     return peak_dataframe
 
 def keep_mz_in_range(peak_dataframe):
     """
-    Filter the peak dataframe to keep only the rows where the 'mz' column value is within the range of 50 and 2000.
+        Keep M/Z in Range
 
-    :param peak_dataframe: A pandas DataFrame containing peaks information.
-    :return: A filtered pandas DataFrame with the 'mz' values within the specified range.
+        Keeps the M/Z values within a specified range in the given peak dataframe.
+
+        :param peak_dataframe: A pandas DataFrame containing peak data.
+        :return: A filtered pandas DataFrame with M/Z values within the specified range.
     """
     peak_dataframe = peak_dataframe.loc[(peak_dataframe['mz'] >= 50) & (peak_dataframe['mz'] <= 2000)]
 
@@ -68,42 +67,38 @@ def keep_mz_in_range(peak_dataframe):
 
 def check_minimum_of_high_peaks_requiered(peak_dataframe, intensity_percent, no_peaks):
     """
-    Check if the number of high peaks in a peak dataframe meets the minimum requirement.
+    Checks if the minimum number of high peaks is met.
 
-    :param peak_dataframe: DataFrame object containing peak data.
-    :param intensity_percent: Intensity percentage threshold for considering peaks as high peaks.
+    :param peak_dataframe: DataFrame containing peak data.
+    :param intensity_percent: Minimum intensity percentage for a peak to be considered high.
     :param no_peaks: Minimum number of high peaks required.
-
-    :return: DataFrame object containing the high peaks if the number of high peaks meets the minimum requirement,
-             None otherwise.
+    :return: DataFrame containing the filtered high peaks if the minimum is met, otherwise an empty DataFrame.
     """
     percent_of_max = peak_dataframe['intensity']/peak_dataframe['intensity'].max() * 100
     filtered_df = peak_dataframe[percent_of_max >= intensity_percent]
     if len(filtered_df) < no_peaks:
-        return None
+        return pd.DataFrame()
     else:
         return filtered_df
 
 def apply_filters(peak_dataframe, precursormz):
     """
-
-    Apply filters on a peak dataframe based on the given parameters.
+    Apply various filters to a peak dataframe.
 
     :param peak_dataframe: A dataframe containing peak information.
     :param precursormz: The precursor m/z value.
-    :return: The filtered peak dataframe.
-
+    :return: A modified peak dataframe after applying filters.
     """
     peak_dataframe = check_minimum_peak_requiered(peak_dataframe)
-    if peak_dataframe is None:
-        return None
+    if peak_dataframe.empty:
+        return pd.DataFrame()
     else:
         peak_dataframe = remove_peak_above_precursormz(peak_dataframe, precursormz)
         peak_dataframe = reduce_peak_list(peak_dataframe)
         peak_dataframe = normalize_intensity(peak_dataframe)
         peak_dataframe = keep_mz_in_range(peak_dataframe)
         peak_dataframe = check_minimum_of_high_peaks_requiered(peak_dataframe, intensity_percent=5.0, no_peaks=2)
-        if peak_dataframe is None:
-            return None
+        if peak_dataframe.empty:
+            return pd.DataFrame()
 
         return peak_dataframe
