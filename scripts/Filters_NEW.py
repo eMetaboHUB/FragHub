6d672e56e11a5255051f7a8205642447f91c1964
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2023-12-15 15:30:51 +0100

    Import pandas in Filters_NEW.py and refactor peak filtering
    Pandas has been imported in Filters_NEW.py to handle data structures more effectively. The parsing and filtering of m/z (mass-to-charge ratio) peaks in msp_parsers.py has been refined by using the 'remove_peak_above_precursormz' function from Filters_NEW module, which enhances the peak data processing.

--- scripts/Filters_NEW.py
+++ scripts/Filters_NEW.py
@@ -1,3 +1,4 @@
+import pandas as pd
 
 def remove_peak_above_precursormz(peak_dataframe, precursormz):
     """
@@ -6,5 +7,10 @@ def remove_peak_above_precursormz(peak_dataframe, precursormz):
     :return: The filtered dataframe with peaks having m/z values below the provided precursor m/z + 5.0 Da.
     """
     peak_dataframe = peak_dataframe[peak_dataframe['mz'] < float(precursormz) + 5.0]  # Removing peaks with mz > precursormz + 5 Da
+
+    return peak_dataframe
+
+def apply_filters(peak_dataframe, precursormz):
+    peak_dataframe = remove_peak_above_precursormz(peak_dataframe, precursormz)
 
     return peak_dataframe
