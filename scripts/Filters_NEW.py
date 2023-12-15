25a78e1e473c44369f55fdeb81c9f6b0dcf0bde6
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2023-12-15 15:14:49 +0100

    Add remove_peak_above_precursormz function in Filters_NEW.py
    The new function, remove_peak_above_precursormz, filters out peaks from the provided dataframe with m/z values exceeding the given precursor m/z + 5.0 Da. The function enhances data specificity and ensures the peak list remains concise and relevant by removing unnecessarily high peaks.

---
+++ scripts/Filters_NEW.py
@@ -0,0 +1,10 @@
+
+def remove_peak_above_precursormz(peak_dataframe, precursormz):
+    """
+    :param peak_dataframe: The dataframe containing peak information.
+    :param precursormz: The precursor m/z value used as a threshold for filtering peaks.
+    :return: The filtered dataframe with peaks having m/z values below the provided precursor m/z + 5.0 Da.
+    """
+    peak_dataframe = peak_dataframe[peak_dataframe['mz'] < float(precursormz) + 5.0]  # Removing peaks with mz > precursormz + 5 Da
+
+    return peak_dataframe
