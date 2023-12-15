75e2f4c37b18883f72a8a7144363a7f902758814
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2023-12-15 15:49:22 +0100

    Add 'check_minimum_peak_required' method in Filters_NEW.py
    A new function 'check_minimum_peak_required' is added to check the count of peaks in the dataframe. If the count is less than 3, it will return 'None'. This check is incorporated in 'apply_filters' function to ensure minimum peaks requirement is met before applying other filters.

--- scripts/Filters_NEW.py
+++ scripts/Filters_NEW.py
@@ -26,22 +26,35 @@ def reduce_peak_list(peak_dataframe):
 
         return peak_dataframe
 
+def check_minimum_peak_requiered(peak_dataframe):
+    """
+    Checks if the given peak_dataframe has the minimum required number of peaks.
+
+    :param peak_dataframe: A dataframe containing peak information.
+    :type peak_dataframe: pandas.DataFrame
+
+    :return: The peak_dataframe if it has at least 3 peaks, None otherwise.
+    :rtype: pandas.DataFrame or None
+    """
+    if len(peak_dataframe) < 3:
+        return None
+    else:
+        return peak_dataframe
 
 def apply_filters(peak_dataframe, precursormz):
     """
-    Apply Filters
+    Apply filters to the peak_dataframe based on the precursormz.
 
-    Applies filters to a peak dataframe based on a given precursor m/z.
+    :param peak_dataframe: A DataFrame containing peak information.
+    :param precursormz: The precursor m/z value used to filter peaks.
 
-    Parameters:
-    :param peak_dataframe: The dataframe containing peak data.
-    :param precursormz: The precursor m/z value to filter the peaks.
-
-    Returns:
-    The filtered peak dataframe.
-
+    :return: A filtered peak_dataframe after applying the necessary filters.
     """
-    peak_dataframe = remove_peak_above_precursormz(peak_dataframe, precursormz)
-    peak_dataframe = reduce_peak_list(peak_dataframe)
+    peak_dataframe = check_minimum_peak_requiered(peak_dataframe)
+    if peak_dataframe is None:
+        return None
+    else:
+        peak_dataframe = remove_peak_above_precursormz(peak_dataframe, precursormz)
+        peak_dataframe = reduce_peak_list(peak_dataframe)
 
-    return peak_dataframe
+        return peak_dataframe
