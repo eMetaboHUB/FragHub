a326788a2d96c6e09c05f71167099e2681b133d7
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2023-12-15 15:33:15 +0100

    Add documentation to apply_filters method
    Added a docstring to the apply_filters method in the Filters_NEW.py script. The docstring provides a brief description of the function, its parameters, and its return value, improving code readability and maintainability.

--- scripts/Filters_NEW.py
+++ scripts/Filters_NEW.py
@@ -11,6 +11,19 @@ def remove_peak_above_precursormz(peak_dataframe, precursormz):
     return peak_dataframe
 
 def apply_filters(peak_dataframe, precursormz):
+    """
+    Apply Filters
+
+    Applies filters to a peak dataframe based on a given precursor m/z.
+
+    Parameters:
+    :param peak_dataframe: The dataframe containing peak data.
+    :param precursormz: The precursor m/z value to filter the peaks.
+
+    Returns:
+    The filtered peak dataframe.
+
+    """
     peak_dataframe = remove_peak_above_precursormz(peak_dataframe, precursormz)
 
     return peak_dataframe
