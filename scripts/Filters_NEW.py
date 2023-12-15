93d0db5d60577887460155bc28543121721b043c
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2023-12-15 15:42:31 +0100

    Implement 'reduce_peak_list' method in Filters_NEW.py
    Added the 'reduce_peak_list' function in Filters_NEW.py script. This function reduces a supplied data frame to the top 500 peaks based on intensity and subsequently sorts the peaks by mz. This enhancement improves peak data handling efficiency and robustness.

--- scripts/Filters_NEW.py
+++ scripts/Filters_NEW.py
@@ -10,6 +10,23 @@ def remove_peak_above_precursormz(peak_dataframe, precursormz):
 
     return peak_dataframe
 
+def reduce_peak_list(peak_dataframe):
+    """
+    :param peak_dataframe: A DataFrame containing peak data.
+    :return: A reduced DataFrame containing the top 500 peaks based on intensity, sorted by mz.
+
+    The reduce_peak_list method takes a peak_dataframe as input and returns a reduced DataFrame containing the top 500 peaks based on intensity. The method first sorts the peak_dataframe
+    * in descending order based on intensity and retains the top 500 rows. It then sorts the resulting DataFrame by mz.
+    """
+    if len(peak_dataframe) > 500:
+        # Triez le DataFrame par intensité en ordre décroissant et gardez les 500 premières lignes
+        peak_dataframe = peak_dataframe.sort_values('intensity', ascending=False).head(500)
+        # Re-classez ensuite par 'mz' si vous le souhaitez
+        peak_dataframe = peak_dataframe.sort_values('mz')
+
+        return peak_dataframe
+
+
 def apply_filters(peak_dataframe, precursormz):
     """
     Apply Filters
@@ -25,5 +42,6 @@ def apply_filters(peak_dataframe, precursormz):
 
     """
     peak_dataframe = remove_peak_above_precursormz(peak_dataframe, precursormz)
+    peak_dataframe = reduce_peak_list(peak_dataframe)
 
     return peak_dataframe
