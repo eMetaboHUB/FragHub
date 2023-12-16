01b6c9b3ac4f83908b36f92d3d481f68ab574d67
Axel <axel.dablanc@univ-tlse3.fr>
2023-12-16 09:08:05 +0100

    Refactor filters for dynamic parameterization
    Refactored filter functions in 'Filters_NEW.py' to accept dynamic parameters. The changes allow runtime configuration of parameters such as the maximum number of peaks, required minimum peaks, and the m/z range, enhancing the script's flexibility. Also updated the function calls to use variables from 'parameters_dict'.

--- scripts/Filters_NEW.py
+++ scripts/Filters_NEW.py
@@ -12,29 +12,29 @@ def remove_peak_above_precursormz(peak_dataframe, precursormz):
 
     return peak_dataframe
 
-def reduce_peak_list(peak_dataframe):
+def reduce_peak_list(peak_dataframe,max_peaks):
     """
     Reduce the peak list to a maximum of 500 peaks.
 
     :param peak_dataframe: The dataframe containing the peak data.
     :return: The reduced peak dataframe.
     """
-    if len(peak_dataframe) > 500:
+    if len(peak_dataframe) > max_peaks:
         # Triez le DataFrame par intensité en ordre décroissant et gardez les 500 premières lignes
-        peak_dataframe = peak_dataframe.sort_values('intensity', ascending=False).head(500)
+        peak_dataframe = peak_dataframe.sort_values('intensity', ascending=False).head(max_peaks)
         # Re-classez ensuite par 'mz' si vous le souhaitez
         peak_dataframe = peak_dataframe.sort_values('mz')
 
     return peak_dataframe
 
-def check_minimum_peak_requiered(peak_dataframe):
+def check_minimum_peak_requiered(peak_dataframe,n_peaks):
     """
     Check if the given peak dataframe meets the minimum requirement.
 
     :param peak_dataframe: A pandas dataframe representing the peak data.
     :return: A pandas dataframe. If the length of the peak dataframe is less than 3, an empty dataframe is returned. Otherwise, the peak dataframe is returned as is.
     """
-    if len(peak_dataframe) < 3:
+    if len(peak_dataframe) < n_peaks:
         return pd.DataFrame()
     else:
         return peak_dataframe
@@ -52,7 +52,7 @@ def normalize_intensity(peak_dataframe):
 
     return peak_dataframe
 
-def keep_mz_in_range(peak_dataframe):
+def keep_mz_in_range(peak_dataframe,mz_from,mz_to):
     """
         Keep M/Z in Range
 
@@ -61,7 +61,7 @@ def keep_mz_in_range(peak_dataframe):
         :param peak_dataframe: A pandas DataFrame containing peak data.
         :return: A filtered pandas DataFrame with M/Z values within the specified range.
     """
-    peak_dataframe = peak_dataframe.loc[(peak_dataframe['mz'] >= 50) & (peak_dataframe['mz'] <= 2000)]
+    peak_dataframe = peak_dataframe.loc[(peak_dataframe['mz'] >= mz_from) & (peak_dataframe['mz'] <= mz_to)]
 
     return peak_dataframe
 
@@ -89,15 +89,30 @@ def apply_filters(peak_dataframe, precursormz):
     :param precursormz: The precursor m/z value.
     :return: A modified peak dataframe after applying filters.
     """
-    peak_dataframe = check_minimum_peak_requiered(peak_dataframe)
+    global parameters_dict
+
+    n_peaks = parameters_dict['check_minimum_peak_requiered_n_peaks']
+    max_peaks = parameters_dict['reduce_peak_list_max_peaks']
+    mz_from = parameters_dict['keep_mz_in_range_from_mz']
+    mz_to = parameters_dict['keep_mz_in_range_to_mz']
+    intensity_percent = parameters_dict['check_minimum_of_high_peaks_requiered_intensity_percent']
+    no_peaks = parameters_dict['check_minimum_of_high_peaks_requiered_no_peaks']
+
+
+    peak_dataframe = check_minimum_peak_requiered(peak_dataframe,n_peaks)
     if peak_dataframe.empty:
         return pd.DataFrame()
     else:
-        peak_dataframe = remove_peak_above_precursormz(peak_dataframe, precursormz)
-        peak_dataframe = reduce_peak_list(peak_dataframe)
-        peak_dataframe = normalize_intensity(peak_dataframe)
-        peak_dataframe = keep_mz_in_range(peak_dataframe)
-        peak_dataframe = check_minimum_of_high_peaks_requiered(peak_dataframe, intensity_percent=5.0, no_peaks=2)
+        if parameters_dict['remove_peak_above_precursormz'] == 1.0:
+            peak_dataframe = remove_peak_above_precursormz(peak_dataframe, precursormz)
+        if parameters_dict['reduce_peak_list'] == 1.0:
+            peak_dataframe = reduce_peak_list(peak_dataframe,max_peaks)
+        if parameters_dict['normalize_intensity'] == 1.0:
+            peak_dataframe = normalize_intensity(peak_dataframe)
+        if parameters_dict['keep_mz_in_range'] == 1.0:
+            peak_dataframe = keep_mz_in_range(peak_dataframe,mz_from,mz_to)
+        if parameters_dict['check_minimum_of_high_peaks_requiered'] == 1.0:
+            peak_dataframe = check_minimum_of_high_peaks_requiered(peak_dataframe, intensity_percent, no_peaks)
         if peak_dataframe.empty:
             return pd.DataFrame()
 
