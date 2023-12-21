ab5863ee5baec96c0190ce0d2bcb8fa576f40321
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2023-12-21 16:27:06 +0100

    Update Excel output path and refactor normalization process
    The Excel output path in "test.py" was updated to "test_gnps_2". The normalization process for metadata in "values_normalizer.py" was refactored by adding "normalize_empties" function and commenting out other ones, making the process more efficient.

--- scripts/values_normalizer.py
+++ scripts/values_normalizer.py
@@ -1,3 +1,17 @@
+import numpy as np
+import re
+
+def normalize_empties(metadata_dict):
+    regex_to_replace = [re.compile(re.escape(str(item)), re.I) for item in [0, 0.0, "", "na", "n/a", "nan", "unknown","unknown","none", np.nan]]
+
+    for k, v in metadata_dict.items():
+        # For each item to replace
+        for regex in regex_to_replace:
+            # If the current value matches, replace it
+            if regex.fullmatch(str(v)):
+                metadata_dict[k] = np.nan
+
+    return metadata_dict
 
 def normalize_values(metadata_dict):
     """
@@ -6,13 +20,13 @@ def normalize_values(metadata_dict):
     """
     metadata_dict = normalize_empties(metadata_dict)
     
-    metadata_dict = normalize_adduct(metadata_dict)
-    metadata_dict = normalize_ionmode(metadata_dict)
-    metadata_dict = normalize_retention_time(metadata_dict)
-    metadata_dict = normalize_ms_level(metadata_dict)
-    metadata_dict = normalize_synonymes(metadata_dict)
-    metadata_dict = normalize_formula(metadata_dict)
-    metadata_dict = normalize_predicted(metadata_dict)
-    metadata_dict = normalize_db_informations(metadata_dict)
+    # metadata_dict = normalize_adduct(metadata_dict)
+    # metadata_dict = normalize_ionmode(metadata_dict)
+    # metadata_dict = normalize_retention_time(metadata_dict)
+    # metadata_dict = normalize_ms_level(metadata_dict)
+    # metadata_dict = normalize_synonymes(metadata_dict)
+    # metadata_dict = normalize_formula(metadata_dict)
+    # metadata_dict = normalize_predicted(metadata_dict)
+    # metadata_dict = normalize_db_informations(metadata_dict)
 
     return metadata_dict
