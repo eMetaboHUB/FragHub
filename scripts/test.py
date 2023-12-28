88100be0b547e35c4f1417fc3d418a8243ea074a
Axel <axel.dablanc@univ-tlse3.fr>
2023-12-28 14:27:39 +0100

    Implement FragHubID generation and compare with originals
    Enhanced the scripts/test.py to include the functionality of generating Fragment Hub IDs (FragHubID) directly from the input MSP files using the fraghudid_generator.py script. Additionally, the changes integrate the functionality of comparing generated FragHubIDs with the original ones utilizing the diff_fraghubid_originals_vs

--- scripts/test.py
+++ scripts/test.py
@@ -1,9 +1,12 @@
 from msp_parsers import *
 from data_preparer import *
 from set_parameters import *
+from fraghubid_generator import *
 import numpy as np
 import time
 import os
+
+from diff_fraghubid_originals_vs_clean import *
 
 ordered_columns = ["FILENAME",
                    "PREDICTED",
@@ -38,30 +41,34 @@ build_window()
 
 start_time = time.time()
 
-# generating filepath list
-spectrum_path_list = []
-MSP_path = os.path.abspath(r"../INPUT/MSP")
-for files in os.listdir(MSP_path):
-    if files.endswith(".msp"):
-        spectrum_path_list.append(os.path.join(MSP_path,files))
+generate_fraghub_id(r"../INPUT/MSP") # one time !!!
 
-spectrum_path_list = [spectrums for spectrums in spectrum_path_list if spectrums != None]
-
-for path in spectrum_path_list:
-    spectrum_list = load_spectrum_list(path)
-    break
-
-spectrum_list = msp_parsing_processing(spectrum_list)
-
-# print(len(spectrum_list))
-
-df = pd.DataFrame(spectrum_list)
-
-df = df[ordered_columns]
-
-
-df.to_excel(rf"C:\Users\Axel\Documents\PYTHON\MSP_V3\FragHub\OUTPUT\MSP\TEST\test_gnps_3.xlsx",index=False)
-
+# # generating filepath list
+# spectrum_path_list = []
+# MSP_path = os.path.abspath(r"../INPUT/MSP")
+# for files in os.listdir(MSP_path):
+#     if files.endswith(".msp"):
+#         spectrum_path_list.append(os.path.join(MSP_path,files))
+#
+# spectrum_path_list = [spectrums for spectrums in spectrum_path_list if spectrums != None]
+#
+# for path in spectrum_path_list:
+#     spectrum_list = load_spectrum_list(path)
+#     break
+#
+# spectrum_list = msp_parsing_processing(spectrum_list)
+#
+# # print(len(spectrum_list))
+#
+# df = pd.DataFrame(spectrum_list)
+#
+# df = df[ordered_columns]
+#
+# fraghub_id_column = pd.DataFrame(columns=['FRAGHUBID'])
+# fraghub_id_column = df[["FRAGHUBID"]]
+#
+# # df.to_excel(rf"C:\Users\Axel\PycharmProjects\msp_v3\OUTPUT\MSP\TEST\test_msms_pos.xlsx",index=False)
+#
 # print("writting soon")
 #
 # chunk_size = 5000  # Taille de chaque fraction
@@ -72,19 +79,21 @@ df.to_excel(rf"C:\Users\Axel\Documents\PYTHON\MSP_V3\FragHub\OUTPUT\MSP\TEST\tes
 #         df_slice = df[start:start + chunk_size]
 #         if start == 0:
 #             # Écrire les en-têtes pour la première fraction
-#             df_slice.to_csv(rf"C:\Users\Axel\PycharmProjects\msp_v3\OUTPUT\MSP\TEST\test_mona_exp.csv", mode='w', sep=";", quotechar='"', encoding="UTF-8", index=False)
+#             df_slice.to_csv(rf"C:\Users\Axel\PycharmProjects\msp_v3\OUTPUT\MSP\TEST\test_mona.csv", mode='w', sep=";", quotechar='"', encoding="UTF-8", index=False)
 #         else:
 #             # Append dans le fichier sans écrire les en-têtes pour les autres fractions
-#             df_slice.to_csv(rf"C:\Users\Axel\PycharmProjects\msp_v3\OUTPUT\MSP\TEST\test_mona_exp.csv", mode='a', header=False, index=False, sep=";", quotechar='"', encoding="UTF-8")
+#             df_slice.to_csv(rf"C:\Users\Axel\PycharmProjects\msp_v3\OUTPUT\MSP\TEST\test_mona.csv", mode='a', header=False, index=False, sep=";", quotechar='"', encoding="UTF-8")
 #
 #         # Mettre à jour la barre de progression
 #         pbar.update()
-
-# compteur = 1
-# for spectrum in spectrum_list:
-#     print(spectrum)
-#     spectrum.to_excel(rf"C:\Users\Axel\PycharmProjects\msp_v3\OUTPUT\MSP\TEST\test_{compteur}.xlsx",index=False)
-#     compteur += 1
-#     break
+#
+# # compteur = 1
+# # for spectrum in spectrum_list:
+# #     print(spectrum)
+# #     spectrum.to_excel(rf"C:\Users\Axel\PycharmProjects\msp_v3\OUTPUT\MSP\TEST\test_{compteur}.xlsx",index=False)
+# #     compteur += 1
+# #     break
+#
+# diff_fraghubid_originals_vs_clean(fraghub_id_column)
 
 print("--- TOTAL TIME: %s ---" % time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time)))
