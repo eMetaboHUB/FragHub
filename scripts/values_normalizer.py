eb7f9d65e8980a19a07b816bc818027ea6e7992c
Axel <axel.dablanc@univ-tlse3.fr>
2023-12-28 15:11:07 +0100

    Remove unused normalizations in value_normalizer.py
    The commented out lines for retention time and formula normalizations in the values_normalizer.py script have been removed. These normalizations were not being used and just caused confusion, making the code cleaner and easier to understand.

--- scripts/values_normalizer.py
+++ scripts/values_normalizer.py
@@ -351,10 +351,8 @@ def normalize_values(metadata_dict):
     if metadata_dict:
          metadata_dict = normalize_adduct(metadata_dict)
          metadata_dict = normalize_ionmode(metadata_dict)
-         # metadata_dict = normalize_retention_time(metadata_dict)
          metadata_dict = normalize_ms_level(metadata_dict)
          metadata_dict = normalize_synonymes(metadata_dict)
-         # metadata_dict = normalize_formula(metadata_dict) # NOTE: dérivé depuis rdkit ==> plus propre
          metadata_dict = normalize_predicted(metadata_dict)
 
     return metadata_dict
