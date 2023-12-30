d15e763676ff996c22ef13b6a48901878f4b7f20
Axel <axel.dablanc@univ-tlse3.fr>
2023-12-30 10:59:01 +0100

    Update setup.py with package dependencies
    The setup.py file has been updated to include essential package dependencies. This ensures that the necessary libraries such as numpy, pandas, tqdm, and rdkit are installed along with the package. This change helps to improve the package setup process and user experience.

--- setup.py
+++ setup.py
@@ -8,5 +8,11 @@ setup(
     license='',
     author='Axel Dablanc',
     author_email='axel.dablanc@univ-tlse3.fr',
-    description=''
+    description='',
+    install_requires=[
+            'numpy~=1.24.4',
+            'pandas~=2.1.0',
+            'tqdm~=4.65.0',
+            'rdkit~=2023.3.3'
+        ]
 )
