7326538f65e3731b2d4a027af688c93b5ff7fa7c
Axel <axel.dablanc@univ-tlse3.fr>
2023-12-30 10:51:38 +0100

    Add setup.py file for package configuration
    Set up.py file has been added to configure the package information. This includes the package name, version, author, and other relevant details. This addition centralizes package information and makes it easier for users to understand the project's basic configuration.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='FragHub',
+    version='4.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Axel Dablanc',
+    author_email='axel.dablanc@univ-tlse3.fr',
+    description=''
+)
