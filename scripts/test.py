b3e2daf3239163a4986db5bd7e4f0bf0fe1b9e68
Axel <axel.dablanc@univ-tlse3.fr>
2024-01-03 09:38:56 +0100

    Refactor scripts/test.py to implement retention time normalization
    The test script has been significantly overhauled with a majority of the previous codes being removed. The main change is the addition of a function to normalize retention time from various formats into a standard one. This should enhance data consistency and make subsequent data processing tasks easier and more efficient. The function handles variations in case, decimal representation, unit representation and absence of units.

--- scripts/test.py
+++ scripts/test.py
@@ -1,106 +1,35 @@
-from msp_normalizer import *
-from set_parameters import *
-from fraghubid_generator import *
-import numpy as np
-import time
-import os
+import re
 
-from diff_fraghubid_originals_vs_clean import *
+metadata_dict = {}
+metadata_dict["RETENTIONTIME"] = "10.517 min"
 
-ordered_columns = ["FILENAME",
-                   "PREDICTED",
-                   "FRAGHUBID",
-                   "SPECTRUMID",
-                   "RESOLUTION",
-                   "SYNON",
-                   "CHARGE",
-                   "IONIZATION",
-                   "MSLEVEL",
-                   "FRAGMENTATIONMODE",
-                   "NAME",
-                   "PRECURSORMZ",
-                   "EXACTMASS",
-                   "AVERAGEMASS",
-                   "PRECURSORTYPE",
-                   "INSTRUMENTTYPE",
-                   "INSTRUMENT",
-                   "SMILES",
-                   "INCHI",
-                   "INCHIKEY",
-                   "COLLISIONENERGY",
-                   "FORMULA",
-                   "RETENTIONTIME",
-                   "IONMODE",
-                   "COMMENT",
-                   "NUM PEAKS",
-                   "peak_list"]
+def normalize_retentiontime(metadata_dict):
+    retientiontime = metadata_dict["RETENTIONTIME"]
 
-# Execution de la fonction
-build_window()
+    match = re.search("(-?\d+[.,]?\d*(?:[Ee][+-]?\d+)?)(?:\W)?(.*)", retientiontime, flags=re.IGNORECASE)
 
-start_time = time.time()
+    if match:
+        time = match.group(1)
+        unit = match.group(2).lower()
 
-# generate_fraghub_id(r"../INPUT/MSP") # one time !!!
-
-# generating filepath list
-spectrum_path_list = []
-MSP_path = os.path.abspath(r"../INPUT/MSP")
-for files in os.listdir(MSP_path):
-    if files.endswith(".msp"):
-        spectrum_path_list.append(os.path.join(MSP_path,files))
-
-spectrum_path_list = [spectrums for spectrums in spectrum_path_list if spectrums != None]
-
-for path in spectrum_path_list:
-    spectrum_list = load_spectrum_list(path)
-    break
-
-spectrum_list = msp_cleaning_processing(spectrum_list)
-
-# print(len(spectrum_list))
-
-df = pd.DataFrame(spectrum_list)
-
-df = df[ordered_columns]
-
-print("-- MOLS HARMONIZATION AND MASS CALCULATION --")
-time.sleep(0.01)
-df = mols_derivation_and_calculation(df)
-
-# print("-- MASS CALCULATION --")
-# time.sleep(0.01)
-# df = mass_calculator(df)
-
-fraghub_id_column = pd.DataFrame(columns=['FRAGHUBID'])
-fraghub_id_column = df[["FRAGHUBID"]]
-
-# df.to_excel(rf"C:\Users\Axel\PycharmProjects\msp_v3\OUTPUT\MSP\TEST\test_msms_pos.xlsx",index=False)
-
-print("writting soon")
-
-chunk_size = 5000  # Taille de chaque fraction
-num_chunks = int(np.ceil(df.shape[0] / chunk_size))  # Calculer le nombre de fractions
-
-with tqdm(total=num_chunks, unit=" row", colour="green", desc="\t    writting") as pbar:
-    for start in range(0, df.shape[0], chunk_size):
-        df_slice = df[start:start + chunk_size]
-        if start == 0:
-            # Écrire les en-têtes pour la première fraction
-            df_slice.to_csv(rf"C:\Users\Axel\PycharmProjects\msp_v3\OUTPUT\MSP\TEST\test_gnps.csv", mode='w', sep=";", quotechar='"', encoding="UTF-8", index=False)
+        if not unit:
+            retientiontime = str(float(time))
+            metadata_dict["RETENTIONTIME"] = retientiontime
+            return metadata_dict
         else:
-            # Append dans le fichier sans écrire les en-têtes pour les autres fractions
-            df_slice.to_csv(rf"C:\Users\Axel\PycharmProjects\msp_v3\OUTPUT\MSP\TEST\test_gnps.csv", mode='a', header=False, index=False, sep=";", quotechar='"', encoding="UTF-8")
+            if unit in ["m", "min", "minute", "minutes"]:
+                retientiontime = str(float(time))
+                metadata_dict["RETENTIONTIME"] = retientiontime
+                return metadata_dict
+            elif unit in ["s", "sec", "second", "seconds"]:
+                retientiontime = str(float(time) / 60)
+                metadata_dict["RETENTIONTIME"] = retientiontime
+                return metadata_dict
+            elif unit in ["ms", "millisecond", "milliseconds"]:
+                retientiontime = str(float(time) / 60000)
+                metadata_dict["RETENTIONTIME"] = retientiontime
+                return metadata_dict
 
-        # Mettre à jour la barre de progression
-        pbar.update()
+    return metadata_dict
 
-# compteur = 1
-# for spectrum in spectrum_list:
-#     print(spectrum)
-#     spectrum.to_excel(rf"C:\Users\Axel\PycharmProjects\msp_v3\OUTPUT\MSP\TEST\test_{compteur}.xlsx",index=False)
-#     compteur += 1
-#     break
-
-diff_fraghubid_originals_vs_clean(fraghub_id_column)
-
-print("--- TOTAL TIME: %s ---" % time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time)))
+print(normalize_retentiontime(metadata_dict))
