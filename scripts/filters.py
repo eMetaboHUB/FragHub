87dc92fe013495e22f68e27237cee8ad12ec4e7f
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2024-01-08 12:14:19 +0100

    Uncomment filtering condition in filters.py
    The function to check for a minimum number of high peaks in the spectrum data was previously commented out in the filters.py script. This update uncommented it, enabling the filtering functionality. Now, if the 'check_minimum_of_high_peaks_required' parameter is set to 1, the corresponding filter is applied to the peak array.

--- scripts/filters.py
+++ scripts/filters.py
@@ -123,10 +123,8 @@ def apply_filters(peak_array, precursormz):
     if parameters_dict['keep_mz_in_range'] == 1.0:
         peak_array = keep_mz_in_range(peak_array, mz_from, mz_to)
 
-    # if parameters_dict['check_minimum_of_high_peaks_requiered'] == 1.0:
-    #     peak_array = check_minimum_of_high_peaks_requiered(peak_array, intensity_percent, no_peaks)
-
-    # peak_array = np.array2string(peak_array)
+    if parameters_dict['check_minimum_of_high_peaks_requiered'] == 1.0:
+        peak_array = check_minimum_of_high_peaks_requiered(peak_array, intensity_percent, no_peaks)
 
     if len(peak_array) == 0:
         return np.array([])
