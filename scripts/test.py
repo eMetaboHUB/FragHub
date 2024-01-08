24517569ddfd4aeaf849ab7c8d2220003f123f8c
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2024-01-08 15:41:24 +0100

    Update MS level parsing in test.py script
    The test.py script has been updated to use re.findall instead of re.search for MS level parsing. This change improves handling of multiple matches within an input string, enhancing flexibility and accuracy in metadata parsing.

--- scripts/test.py
+++ scripts/test.py
@@ -1,9 +1,15 @@
 import re
 
-texte = "MS2 Composite"
+texte = "MS2-MS3 Composite"
 
 ms_level_pattern = re.compile("(?:ms)?(\d)", flags=re.IGNORECASE)
 
-test = re.findall(ms_level_pattern, texte)
+ms_level = re.findall(ms_level_pattern, texte)
+if ms_level:
+    print(ms_level)
+    if len(ms_level) == 1:
+        test = ms_level[0]
+    elif len(ms_level) >= 2:
+        test = f"{ms_level[0]}-{ms_level[1]}"
 
 print(test)
