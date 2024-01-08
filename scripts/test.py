92ce9ffe0130d604f3164fc9fb6e67d64f471d1a
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2024-01-08 15:00:01 +0100

    Replace retention time normalization with MS level pattern search
    The retention time normalization function and related codes have been completely removed from the test.py script. Instead, a new approach is introduced that detects MS level from text via regular expression. This commit significantly simplifies the script and focus on MS level detection from given text.

--- scripts/test.py
+++ scripts/test.py
@@ -1,35 +1,9 @@
 import re
 
-metadata_dict = {}
-metadata_dict["RETENTIONTIME"] = "10.517 min"
+texte = "MS2 Composite"
 
-def normalize_retentiontime(metadata_dict):
-    retientiontime = metadata_dict["RETENTIONTIME"]
+ms_level_pattern = re.compile("(?:ms)?(\d)", flags=re.IGNORECASE)
 
-    match = re.search("(-?\d+[.,]?\d*(?:[Ee][+-]?\d+)?)(?:\W)?(.*)", retientiontime, flags=re.IGNORECASE)
+test = re.findall(ms_level_pattern, texte)
 
-    if match:
-        time = match.group(1)
-        unit = match.group(2).lower()
-
-        if not unit:
-            retientiontime = str(float(time))
-            metadata_dict["RETENTIONTIME"] = retientiontime
-            return metadata_dict
-        else:
-            if unit in ["m", "min", "minute", "minutes"]:
-                retientiontime = str(float(time))
-                metadata_dict["RETENTIONTIME"] = retientiontime
-                return metadata_dict
-            elif unit in ["s", "sec", "second", "seconds"]:
-                retientiontime = str(float(time) / 60)
-                metadata_dict["RETENTIONTIME"] = retientiontime
-                return metadata_dict
-            elif unit in ["ms", "millisecond", "milliseconds"]:
-                retientiontime = str(float(time) / 60000)
-                metadata_dict["RETENTIONTIME"] = retientiontime
-                return metadata_dict
-
-    return metadata_dict
-
-print(normalize_retentiontime(metadata_dict))
+print(test)
