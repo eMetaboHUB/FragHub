2ae7bbfd347a4dae70b7fb7f476e40af94508baf
Axel <axel.dablanc@univ-tlse3.fr>
2024-01-10 17:36:35 +0100

    Remove unused regex and functions in msp_normalizer.py
    This commit simplifies the `msp_normalizer.py` script by removing multiple unused regular expressions and several unused functions. This refactor streamlines the process and improves maintainability by leaning more into pre-built library methods, particularly for handling JSON data. Also ensured error handling in case of failed spectrum parsing.

--- scripts/msp_normalizer.py
+++ scripts/msp_normalizer.py
@@ -44,122 +44,6 @@ keys_list = ['FILENAME',
              'NUM PEAKS',
              'PEAKS_LIST']
 
-global metadata_peak_list_split_pattern
-metadata_peak_list_split_pattern = re.compile(r"([\s\S]*:.[0-9]*\n)(((-?\d+[.,]?\d*(?:[Ee][+-]?\d+)?)(\s+|:)(-?\d+[.,]?\d*(?:[Ee][+-]?\d+)?)(.*)(\n|$))*)")
-
-global sub_fields_pattern
-sub_fields_pattern = re.compile(r"(\S+?)=\"([^\"]*)\"|\"(\w+?)=([^\"]*)\"|\"([^\"]*?)=([^\"]*)\"|(\S+?)=(\d+(?:[.,]\d*)?)|(\S+?)=(.*?)(?:;|\n|$)")
-
-global metadata_pattern
-metadata_pattern = re.compile(r"([^:\n]*?):\s*([^:\n]*)(?:\n|$)")
-
-global metadata_fields_name_pattern
-metadata_fields_name_pattern = re.compile(r'^[\W_]+|[\W_]+$')
-
-global metadata_strip_value_pattern
-metadata_strip_value_pattern = re.compile(r"^\"|\"$")
-
-global peak_list_split_pattern
-peak_list_split_pattern = re.compile(r"(-?\d+\.?\d*(?:[Ee][+-]?\d+)?)(?:\s+|:)(-?\d+[.,]?\d*(?:[Ee][+-]?\d+)?)")
-
-global computed_pattern
-computed_pattern = re.compile(r"computed", flags=re.IGNORECASE)
-
-global comment_pattern
-comment_pattern = re.compile(r'comment.*', flags=re.IGNORECASE)
-
-def load_spectrum_list_json(json_file_path):
-    """
-    Load spectra from a JSON file and return a list of spectra.
-
-    :param json_file_path: Path to the JSON file.
-    :return: List of spectra.
-    """
-    spectrum_list = []
-
-    # First, calculate total bytes for tqdm
-    total_bytes = os.path.getsize(json_file_path)
-
-    with open(json_file_path, 'r', encoding="UTF-8") as file:
-        # ijson.items(file, 'item') returns a generator yielding items in a JSON file
-        spectra = ijson.items(file, 'item')
-        # Create tqdm progress bar
-        progress = tqdm(total=total_bytes, unit="B", unit_scale=True, colour="green", desc="{:>80}".format("Loading file"))
-
-        for spectrum in spectra:
-            spectrum_list.append(spectrum)
-            progress.update(len(str(spectrum)))
-        progress.close()
-
-    return spectrum_list
-
-def extract_metadata_and_peak_list(spectrum):
-    """
-    :param spectrum: The spectrum string containing both metadata and peak list
-    :return: A tuple containing the extracted metadata and peak list
-
-    Extracts the metadata and peak list from the given spectrum string. The spectrum string should be in a specific format where the metadata appears before the peak list. The method uses
-    * regular expressions to find and extract the metadata and peak list.
-
-    The regular expression pattern used to search for the metadata and peak list is:
-    "([\s\S]*:.[0-9]*\n)(((-?\d+\.?\d*(?:[Ee][+-]?\d+)?)(\s+|:)(-?\d+\.?\d*(?:[Ee][+-]?\d+)?)(.*)(\n|$))*)"
-
-    The method searches for this pattern in the spectrum string and if a match is found, it extracts the metadata and peak list.
-
-    Example usage:
-    spectrum = "Metadata: 123\n1 2\n3 4\n5 6"
-    metadata, peak_list = extract_metadata_and_peak_list(spectrum)
-    print(metadata)  # Output: "Metadata: 123"
-    print(peak_list)  # Output: "1 2\n3 4\n5 6"
-    """
-    metadata,peak_list = None,None
-
-    match = re.search(metadata_peak_list_split_pattern, spectrum)
-
-    if match:
-        metadata, peak_list = match.group(1), match.group(2)
-
-    return metadata,peak_list
-
-def check_for_metadata_in_comments(metadata_matches):
-    """
-    :param metadata_matches: A list containing matches of metadata found in comments.
-    :return: A new list of metadata matches. Returns False if no metadata matches found.
-
-    This method checks if comment fields exist in each metadata match. If a comment field exists, it further checks if there are sub-fields within the comment field. The sub-fields are extracted
-    * using regular expressions and added to the new_metadata_matches list. If no sub-fields are found, the entire match is added to the new_metadata_matches list.
-
-    If no metadata matches are found, the method returns False.
-
-    Example usage:
-    metadata_matches = [('comment', 'field1=value1; field2="value2"'), ('metadata', 'field1=value1; field2=value2')]
-
-    result = check_for_metadata_in_comments(metadata_matches)
-    print(result)
-    # Output: [('comment', 'field1=value1'), ('comment', 'field2="value2"')]
-
-    """
-    new_metadata_matches = []
-    # Check if comment filed exist
-    for match in metadata_matches:
-        if re.search(comment_pattern, match[0]):
-            new_metadata_matches.append(match)
-            if "=" in match[1]:
-                sub_fields_matches = re.findall(sub_fields_pattern, match[1])
-                if sub_fields_matches:
-                    for sub_fields_match in sub_fields_matches:
-                        non_empty_tuple = tuple(group for group in sub_fields_match if group)
-                        if len(non_empty_tuple) == 2:
-                            new_metadata_matches.append(non_empty_tuple)
-                else:
-                    return False
-            else:
-                new_metadata_matches.append(match)
-        else:
-            new_metadata_matches.append(match)
-
-    return new_metadata_matches if new_metadata_matches else False
-
 def convert_keys(metadata_dict):
     """
     Convert keys in metadata_dict based on the provided keys_dict and keys_list.
@@ -174,35 +58,6 @@ def convert_keys(metadata_dict):
     converted.update({key: "" for key in keys_list if key not in converted})
 
     return converted
-
-def metadata_to_dict(metadata):
-    """
-    Convert metadata string to DataFrame.
-
-    :param metadata: The metadata string to be converted.
-    :type metadata: str
-    :return: DataFrame containing metadata keys and values.
-    :rtype: pandas.DataFrame
-    """
-    metadata_dict = {}
-
-    metadata_matches = re.findall(metadata_pattern,metadata)
-
-    if metadata_matches:
-        temp = check_for_metadata_in_comments(metadata_matches)
-        temp = [t for t in temp if not re.search(computed_pattern, t[0])]
-        if temp != False:
-            metadata_matches = temp
-
-        for match in metadata_matches:
-            metadata_dict[re.sub(metadata_fields_name_pattern, '', match[0]).lower().strip()] = re.sub(metadata_strip_value_pattern,"",match[1])
-
-        metadata_dict = convert_keys(metadata_dict)
-        metadata_dict = normalize_values(metadata_dict)
-
-        return metadata_dict
-
-    return metadata_dict
 
 def peak_list_to_np_array(peak_list, precursormz):
     """
@@ -241,48 +96,6 @@ def peak_list_to_str(peak_list_np):
 
     return peak_list_np
 
-def structure_metadata_and_peak_list(metadata, peak_list):
-    """
-    Structure metadata and peak list into formatted DataFrames.
-
-    :param metadata: The metadata to be structured.
-    :type metadata: list or tuple
-    :param peak_list: The peak list to be structured.
-    :type peak_list: list or tuple
-    :return: Tuple containing the structured metadata and peak list as DataFrames.
-    :rtype: tuple
-    """
-    if not metadata or not peak_list:
-        return {},''
-    else:
-        metadata_dict = metadata_to_dict(metadata)
-        if not metadata_dict:
-            return {},''
-        if "PRECURSORMZ" in metadata_dict:
-            if metadata_dict["PRECURSORMZ"]:
-                try:
-                    peak_list_np = peak_list_to_np_array(peak_list, float(metadata_dict["PRECURSORMZ"].replace(",", ".")))
-                    peak_list_np = peak_list_to_str(peak_list_np)
-                    return metadata_dict, peak_list_np
-                except:
-                    return {},''
-            else:
-                return {},''
-        else:
-            return {},''
-
-def parse_metadata_and_peak_list(spectrum):
-    """
-    Parse metadata and peak list from a given spectrum.
-
-    :param spectrum: A spectrum object or data structure that contains both metadata and peak list.
-    :return: A tuple containing dataframes for the parsed metadata and peak list.
-    """
-    metadata, peak_list = extract_metadata_and_peak_list(spectrum)
-    metadata_DF, peak_list_str = structure_metadata_and_peak_list(metadata, peak_list)
-
-    return metadata_DF, peak_list_str
-
 def msp_parser(spectrum):
     """
     Parse the given spectrum to extract metadata and peak list.
@@ -292,6 +105,9 @@ def msp_parser(spectrum):
     """
     spectrum = convert_keys(spectrum)
     spectrum = normalize_values(spectrum)
+    if not spectrum:
+        return {}
+
     if "PRECURSORMZ" in spectrum:
         if spectrum["PRECURSORMZ"]:
             try:
