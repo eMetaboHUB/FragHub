46dbc64b09e6e0fef4252efabb70e3104aea5e8f
Axel <axel.dablanc@univ-tlse3.fr>
2024-01-10 17:15:33 +0100

    Validate data in msp_to_json.py converter
    Added a data validity check in msp_to_json.py script. If either metadata or peak_list does not exist, the script will now return None to prevent potential issues or errors later in the process.

--- scripts/convertors/msp_to_json.py
+++ scripts/convertors/msp_to_json.py
@@ -162,6 +162,8 @@ def msp_to_json(spectrum):
     """
     metadata, peak_list = extract_metadata_and_peak_list(spectrum)
     metadata_DF, peak_list = structure_metadata_and_peak_list(metadata, peak_list)
+    if not metadata or not peak_list:
+        return None
     metadata_DF["peaks"] = str(peak_list)
 
     return metadata_DF
