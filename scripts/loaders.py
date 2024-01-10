7837f4ef23fea0d8026ba9b1756941fa2f07da6d
Axel <axel.dablanc@univ-tlse3.fr>
2024-01-10 18:43:48 +0100

    Add loaders.py with spectrum data loading functions
    The new file loaders.py has been added, containing functions to load spectrum data from both MSP and JSON files. The implemented code includes well-documented functions, showcasing usage examples, and implementing data loading with efficient progress tracking. It improves the flexibility of spectrum data processing and handling in the project.

---
+++ scripts/loaders.py
@@ -0,0 +1,65 @@
+from tqdm import tqdm
+import ijson
+import os
+
+def load_spectrum_list_from_msp(msp_file_path):
+    """
+    Load a spectrum list from a given MSP (Mass Spectral Peak) file.
+
+    :param msp_file_path: The path to the MSP file.
+    :return: The list of spectra read from the file. Each spectrum is represented as a string.
+
+    Example usage:
+    ```
+    msp_file_path = "path/to/spectrum.msp"
+    spectrum_list = load_spectrum_list(msp_file_path)
+    print(spectrum_list)
+    ```
+    """
+    filename = os.path.basename(msp_file_path)
+    spectrum_list = []
+    buffer = []
+
+    total_lines = sum(1 for line in open(msp_file_path, 'r', encoding="UTF-8")) # count the total number of lines in the file
+
+    with open(msp_file_path, 'r', encoding="UTF-8") as file:
+        for line in tqdm(file, total=total_lines, unit=" rows", colour="green", desc="{:>80}".format(f"loading [{filename}]")): # wrap this with tqdm
+            if line.strip() == '':
+                if buffer:
+                    spectrum_list.append('\n'.join(buffer))
+                    buffer = []
+            else:
+                if not buffer:
+                    buffer.append(f"FILENAME: {os.path.basename(msp_file_path)}") # adding filename to spectrum
+                buffer.append(line.strip())
+
+    # Add the last spectrum to the list
+    if buffer:
+        spectrum_list.append('\n'.join(buffer))
+
+    return spectrum_list
+
+def load_spectrum_list_json(json_file_path):
+    """
+    Load spectra from a JSON file and return a list of spectra.
+
+    :param json_file_path: Path to the JSON file.
+    :return: List of spectra.
+    """
+    spectrum_list = []
+
+    # First, calculate total bytes for tqdm
+    total_bytes = os.path.getsize(json_file_path)
+
+    with open(json_file_path, 'r', encoding="UTF-8") as file:
+        # ijson.items(file, 'item') returns a generator yielding items in a JSON file
+        spectra = ijson.items(file, 'item')
+        # Create tqdm progress bar
+        progress = tqdm(total=total_bytes, unit="B", unit_scale=True, colour="green", desc="{:>80}".format("Loading file"))
+
+        for spectrum in spectra:
+            spectrum_list.append(spectrum)
+            progress.update(len(str(spectrum)))
+        progress.close()
+
+    return spectrum_list
