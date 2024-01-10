cb6baec24b6114c7e5ee7544ff8359bf3e5fdcd8
Axel <axel.dablanc@univ-tlse3.fr>
2024-01-10 15:57:08 +0100

    Refactor test.py to use ijson for JSON parsing
    The previous version of the script utilized regular expressions for text parsing. This update refactors the code to use ijson for handling JSON files instead, providing a more efficient and reliable approach. The refactored code defines a function to retrieve the first n items from a given JSON file. This change will allow the code to handle large JSON files more effectively.

--- scripts/test.py
+++ scripts/test.py
@@ -1,33 +1,16 @@
-import re
+import ijson
 
-whole_text = """
-NAME: 3-Des-Microcystein_LR
-PRECURSORMZ: 981.54
-PRECURSORTYPE: M+H
-FORMULA: C48H72N10O12
-Ontology: 
-INCHIKEY: IYDKWWDUBYWQGF-NNAZGLEUSA-N
-INCHI: N/A
-SMILES: CC(C)CC1NC(=O)C(C)NC(=O)C(=C)N(C)C(=O)CCC(NC(=O)C(C)C(NC(=O)C(CCCNC(N)=N)NC(=O)C(C)C(NC1=O)C(O)=O)\C=C\C(\C)=C\C(C)C(O)Cc1ccccc1)C(O)=O
-RETENTIONTIME: CCS: 
-IONMODE: Positive
-INSTRUMENTTYPE: LC-ESI-qTof
-INSTRUMENT: qTof
-COLLISIONENERGY: 
-Comment: DB#=CCMSLIB00000001547; origin=GNPS
-Num Peaks: 218
-289.286377    8068.0
-295.545288    22507.0
-298.489624    3925.0
-317.324951    18742.0
-319.655945    8604.0
-"""
+def get_first_n_items(json_file_path, n):
+    items = []
+    with open(json_file_path, 'r') as f:
+        objects = ijson.items(f, 'item')
+        for index, item in enumerate(objects):
+            if index == n:
+                break
+            items.append(item)
+    return items
 
-# L'expression régulière modifiée pour prendre en compte les espaces dans la clé et les caractères spéciaux comme '#'
-pattern = re.compile(r"([^:\n]*?):\s*([^:\n]*)(?:\n|$)", re.MULTILINE)
-matches = pattern.findall(whole_text)
+first_10_items = get_first_n_items(r"C:\Users\Axel\PycharmProjects\msp_v3\INPUT\JSON\MSP_converted.json", 10)
 
-for match in matches:
-    print("Key: " + match[0].strip())  # Les fonctions strip() sont utilisées pour retirer les espaces en début et en fin de chaîne
-    print("Value: " + match[1].strip())
-    print("\n")
+for item in first_10_items:
+    print(item)
