b30081114eeefe33fbef58bc7b306663e069db8c
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2024-01-16 14:15:25 +0100

    Add HMDB completion function in xml_to_json conversion
    Included a new function "complete_HMDB" in the conversion script which updates missing HMDB information from the spectrum JSON data directly. This advancement enhances efficiency as file IO operations are minimized. It further enhances modularity by integrating well with other processes handling spectra in dictionary format.

--- scripts/convertors/xml_to_json.py
+++ scripts/convertors/xml_to_json.py
@@ -1,3 +1,4 @@
+from ..HMDB_completion import *
 import concurrent.futures
 from tqdm import tqdm
 import re
@@ -7,6 +8,9 @@ metadata_pattern = re.compile(r"^  <(.*?)>(.*?)(?:</.*?>)\n")
 
 global peak_list_pattern
 peak_list_pattern = re.compile(r"^      <mass-charge>(.*?)</mass-charge>\n      <intensity>(.*?)</intensity>")
+
+global hmdb_pattern
+hmdb_pattern = re.compile(r"HMDB.*")
 
 def structure_metadata_and_peak_list(metadata, peak_list):
     """
@@ -33,6 +37,20 @@ def extract_metadata_and_peak_list(spectrum):
 
     return metadata, peak_list
 
+def complete_HMDB(spectrum_json):
+    """
+    Completes the HMDB information in the spectrum JSON.
+
+    :param spectrum_json: The spectrum JSON object to complete.
+    :type spectrum_json: dict
+    :return: The completed spectrum JSON object.
+    :rtype: dict
+    """
+    if re.search(hmdb_pattern, spectrum_json["filename"]):
+        spectrum_json = complete_HMDB_spectrum(spectrum_json)
+
+    return spectrum_json
+
 def xml_to_json(spectrum):
     """
     Converts XML formatted spectrum data to JSON format.
@@ -42,6 +60,7 @@ def xml_to_json(spectrum):
     """
     metadata, peak_list = extract_metadata_and_peak_list(spectrum)
     spectrum_json = structure_metadata_and_peak_list(metadata, peak_list)
+    spectrum_json = complete_HMDB(spectrum_json)
 
     return spectrum_json
 
