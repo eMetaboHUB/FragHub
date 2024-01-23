1fe291cc7aaf3472cf604322db1a8b482309b9e6
Axel <axel.dablanc@univ-tlse3.fr>
2024-01-23 14:04:16 +0100

    Refactor key conversion functionality into separate module
    The key conversion functionality in msp_normalizer.py has been refactored out into a new module, keys_convertor.py to enhance code organization. The keys_convertor.py module handles the key conversions from CSV file to dictionary, and the function "convert_keys" to make reusability easier since it's a common operation required in many scripts.

--- scripts/msp_normalizer.py
+++ scripts/msp_normalizer.py
@@ -1,3 +1,4 @@
+from convertors.keys_convertor import *
 from values_normalizer import *
 from ast import literal_eval
 import concurrent.futures
@@ -12,54 +13,8 @@ import re
 
 np.set_printoptions(suppress=True)
 
-global keys_dict
-Key_dataframe = pd.read_csv(os.path.abspath("../datas/key_to_convert.csv"),sep=";", encoding="UTF-8") # Remplacez 'your_file.csv' par le chemin de votre fichier
-keys_dict = dict(zip(Key_dataframe['known_synonym'], Key_dataframe['fraghub_default'].str.upper()))
-
-global keys_list
-keys_list = ['FILENAME',
-             'PREDICTED',
-             'FRAGHUBID',
-             'SPECTRUMID',
-             'RESOLUTION',
-             'SYNON',
-             'CHARGE',
-             'IONIZATION',
-             'MSLEVEL',
-             'FRAGMENTATIONMODE',
-             'NAME',
-             'PRECURSORMZ',
-             'EXACTMASS',
-             'AVERAGEMASS',
-             'PRECURSORTYPE',
-             'INSTRUMENTTYPE',
-             'INSTRUMENT',
-             'SMILES',
-             'INCHI',
-             'INCHIKEY',
-             'COLLISIONENERGY',
-             'FORMULA',
-             'RETENTIONTIME',
-             'IONMODE',
-             'COMMENT',
-             'NUM PEAKS',
-             'PEAKS_LIST']
-
 global float_check_pattern
 float_check_pattern = re.compile(r"(-?\d+[.,]?\d*(?:[Ee][+-]?\d+)?)")
-
-def convert_keys(metadata_dict):
-    """
-    Convert keys in metadata_dict based on the provided keys_dict and keys_list.
-
-    :param metadata_dict: A dictionary containing metadata information.
-    :return: A dictionary with converted keys based on the provided keys_dict and keys_list.
-    """
-    converted = {keys_dict[key.lower()]: val for key, val in metadata_dict.items() if key.lower() in keys_dict and keys_dict[key.lower()] in keys_list}
-
-    converted.update({key: "" for key in keys_list if key not in converted})
-
-    return converted
 
 def peak_list_to_np_array(peak_list, precursormz):
     """
