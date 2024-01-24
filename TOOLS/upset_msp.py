6238b22eeb723a7aa82e70d5a4f5f0b790cc3a7a
Axel <axel.dablanc@univ-tlse3.fr>
2024-01-24 09:11:28 +0100

    Refactor upset_msp.py and improve data handling
    This update brings changes mainly to 'TOOLS/upset_msp.py': it updates path to match the modified project structure, incorporates error handling during file reading and modifies the method to update 'FILENAME' and 'INCHIKEY' columns. The refactor further enhances performance and reliability of the upset_msp.py script.

--- TOOLS/upset_msp.py
+++ TOOLS/upset_msp.py
@@ -2,27 +2,32 @@ from upsetplot import *
 from matplotlib import pyplot
 import pandas as pd
 import os
+import re
 
-path= r"C:\Users\Axel\Documents\Présentations\MSP\datas diagram\Oct2023"
+path= r"C:\Users\Axel\PycharmProjects\msp_v3\TOOLS\Generate_csv_from_original_files\output"
 
 concatenated_df = pd.DataFrame()
 
 for files in os.listdir(path):
     if files.endswith(".csv") and files != "concats.csv":
-        df = pd.read_csv(os.path.join(path,files), sep=";", encoding="UTF-8")
+        try:
+            df = pd.read_csv(os.path.join(path,files), sep=";",quotechar='"', encoding="UTF-8")
 
-        concatenated_df = pd.concat([concatenated_df, df], axis=0)
+            concatenated_df = pd.concat([concatenated_df, df], axis=0)
+        except:
+            continue
 
 concatenated_df = concatenated_df[['FILENAME', 'INCHIKEY']]
 
 # Utilisez la méthode rename() avec des expressions régulières
-concatenated_df["FILENAME"] = concatenated_df["FILENAME"].replace(r'.*MSMS_.*', 'MSMS_Public', regex=True)
+concatenated_df["FILENAME"] = concatenated_df["FILENAME"].replace(r'.*MSMS_.*', 'RIKEN', regex=True)
 concatenated_df["FILENAME"] = concatenated_df["FILENAME"].replace(r'.*MoNA.*', 'MoNA', regex=True)
 concatenated_df["FILENAME"] = concatenated_df["FILENAME"].replace(r'.*XML_.*', 'HMDB', regex=True)
 concatenated_df["FILENAME"] = concatenated_df["FILENAME"].replace(r'.*MassBank.*', 'MassBank', regex=True)
 concatenated_df["FILENAME"] = concatenated_df["FILENAME"].replace(r'.*ALL_GNPS.*', 'GNPS', regex=True)
 concatenated_df["FILENAME"] = concatenated_df["FILENAME"].replace(r'.*HMDB.*', 'HMDB', regex=True)
 
+concatenated_df['INCHIKEY'] = concatenated_df['INCHIKEY'].apply(lambda x: re.match(r'^(\w{14})-.*$', str(x))[1])
 
 # Utilisez pivot_table pour réorganiser les données
 concatenated_df = concatenated_df.pivot_table(index='INCHIKEY', columns='FILENAME', aggfunc='first', fill_value='')
@@ -39,7 +44,7 @@ pivot_df = pivot_df.fillna('')
 
 pivot_df = pivot_df.drop('INCHIKEY', axis=1)
 
-print(pivot_df)
+# print(pivot_df)
 
 UpSet(pivot_df)
 
