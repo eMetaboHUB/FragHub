f1fb22514079878d0270860768b2002e8e2416b2
Axel <axel.dablanc@univ-tlse3.fr>
2024-01-27 10:41:53 +0100

    Convert all dataframe columns to string in csv_to_msp.py
    The update includes a new line of code to convert all columns in a dataframe to string format in the csv_to_msp.py script. This ensures that subsequent operations on the dataframe are performed correctly without data type mismatch issues.

--- scripts/convertors/csv_to_msp.py
+++ scripts/convertors/csv_to_msp.py
@@ -33,6 +33,9 @@ def dataframe_to_msp(dataframe, name):
         name = "Spectrum Conversion"  # Specify a name for the spectrum
         spectra = dataframe_to_msp(dataframe, name)  # Convert DataFrame to list of formatted spectra strings
     """
+    # convert all columns of dataframe to string
+    dataframe = dataframe.astype(str)
+
     spectrum_list = []
     for index, row in tqdm(dataframe.iterrows(), total=len(dataframe), desc="{:>70}".format(name), colour="green", unit=" row"):
         COMMENTS = format_comments(row)
