4ccce8d8bf9161b01e2a1de78a6a3107aab10b34
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2024-01-31 11:22:24 +0100

    Improve docstrings in RestNPClass script
    The commit rewrites and enhances the quality of docstrings for each function in the RestNPClass.py script. This makes the codebase more comprehensible and easier to understand. It incorporates parameters, return values, and brief explanations of what each function is expected to execute, providing clarity for understanding the script's operation.

--- TOOLS/Genertate_classyfire_and_NPclass/RestNPClass.py
+++ TOOLS/Genertate_classyfire_and_NPclass/RestNPClass.py
@@ -4,13 +4,9 @@ import pandas as pd
 
 
 def checkRequest(r):
-    """[Check if API response is OK (status code 200)]
-
-    Args:
-        r ([type]): API response
-
-    Returns:
-        [bool]: [False if status code is different than 200 else True]
+    """
+    :param r: The response object from an HTTP request
+    :return: True if the response has a status code of 200, False otherwise
     """
     if r.status_code !=200 :
         print(r.status_code)
@@ -19,6 +15,14 @@ def checkRequest(r):
     return True
 
 def getNPClassifFromSmile(smile):
+    """
+    Retrieves the NPClassifier structure from the given SMILE.
+
+    :param smile: The SMILE string of the desired compound.
+    :type smile: str
+    :return: The NPClassifier structure if successfully retrieved, False otherwise.
+    :rtype: dict or bool
+    """
     r = get('https://npclassifier.ucsd.edu/classify?smiles=%s'%(smile))
     if checkRequest(r):
         NPClassif=r.json()
@@ -28,6 +32,16 @@ def getNPClassifFromSmile(smile):
         return False 
 
 def readNPClassif(NPCresults):
+    """
+
+    Method: readNPClassif
+
+    This method takes in one parameter, NPCresults, and returns a dictionary.
+
+    :param NPCresults: A dictionary containing the classification results for NPClassif
+    :return: A dictionary with the updated classification results
+
+    """
     if NPCresults == False:
         return {'class_results': None, 'superclass_results': None, 'pathway_results': None, 'isglycoside': None}
     res = {
@@ -39,6 +53,12 @@ def readNPClassif(NPCresults):
     return res
 
 def addNPClassif(dfcpd, SMILES_col_name="First(Smiles (canonical SMILES))"):
+    """
+    :param dfcpd: The Pandas DataFrame object containing the data to be classified
+    :param SMILES_col_name: The column name in the DataFrame that contains the SMILES string. Default value is "First(Smiles (canonical SMILES))"
+    :return: The modified DataFrame with additional columns containing the NPClassif results for each SMILES string
+
+    """
     for ind in dfcpd.index:
         NPCresults=readNPClassif(getNPClassifFromSmile(dfcpd[SMILES_col_name][ind]))
         for key in NPCresults.keys():
@@ -60,7 +80,12 @@ def addNPclassif(dfcpd):
     return dfcpd
 """
 def convertstr2list(string):
-    
+    """
+    Convert a string representation of a list to a list.
+
+    :param string: A string representation of a list.
+    :return: A list.
+    """
     if type(string)!=str:
         string = str(string).replace("nan","")
     if type(string)==str:
@@ -73,6 +98,12 @@ def convertstr2list(string):
     return res
 
 def convertlist2singlestring(list):
+    """
+    Convert a list of strings into a single string separated by a '|'.
+
+    :param list: A list of strings.
+    :return: A single string consisting of the elements of the list separated by '|'. Returns None if the input list is None.
+    """
     if str(list)!="None":
         return '|'.join(list)
     else :
