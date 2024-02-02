e5021680f0267ee8341ada9da1da598c051e2a7d
Axel <axel.dablanc@univ-tlse3.fr>
2024-02-02 11:56:37 +0100

    Simplify color generation in Python script
    The generate_colors_dict.py script in the TOOLS directory is refactored to simplify the color generation process. It now uses the inbuilt 'colorsys' and 'colors' modules from matplotlib library for generating a dictionary of unique RGB colors, instead of the previous complex method of color conversion. The list of categories has also been modified and cleaned up.

--- TOOLS/generate_colors_dict.py
+++ TOOLS/generate_colors_dict.py
@@ -1,35 +1,10 @@
-import matplotlib
-import matplotlib.cm as cm
-import hashlib
-import numpy as np
+from matplotlib import colors
+from colorsys import hsv_to_rgb
 
-liste = ['Polyketides|Terpenoids', 'Polyketides', 'Alkaloids', 'Shikimates and Phenylpropanoids', 'Terpenoids',
-         'Fatty acids', 'Amino acids and Peptides|Shikimates and Phenylpropanoids', 'Amino acids and Peptides',
-         'Carbohydrates', 'Alkaloids|Shikimates and Phenylpropanoids', 'Carbohydrates|Shikimates and Phenylpropanoids',
-         'Fatty acids|Polyketides', 'Polyketides|Shikimates and Phenylpropanoids',
-         'Amino acids and Peptides|Fatty acids', 'Alkaloids|Amino acids and Peptides', 'Alkaloids|Terpenoids',
-         'Amino acids and Peptides|Polyketides', 'Amino acids and Peptides|Fatty acids|Shikimates and Phenylpropanoids',
-         'Alkaloids|Polyketides', 'Alkaloids|Amino acids and Peptides|Shikimates and Phenylpropanoids',
-         'Shikimates and Phenylpropanoids|Terpenoids', 'Fatty acids|Terpenoids', 'Carbohydrates|Fatty acids',
-         'Carbohydrates|Polyketides', 'Alkaloids|Carbohydrates', 'Amino acids and Peptides|Fatty acids|Polyketides',
-         'Amino acids and Peptides|Carbohydrates', 'Alkaloids|Fatty acids', 'Fatty acids|Polyketides|Terpenoids']
-
-# Récupérer valeurs uniques
+liste = ['Lipids and lipid-like molecules', 'Phenylpropanoids and polyketides', 'Organic acids and derivatives', 'Benzenoids', 'Organoheterocyclic compounds', 'Organohalogen compounds', 'Organic oxygen compounds', 'Organic nitrogen compounds', 'Nucleosides, nucleotides, and analogues', 'Lignans, neolignans and related compounds', 'Organosulfur compounds', 'Alkaloids and derivatives', 'Organophosphorus compounds', 'Hydrocarbon derivatives', 'Hydrocarbons', 'Homogeneous non-metal compounds', 'Organic 1,3-dipolar compounds', 'Organic Polymers', 'Organometallic compounds']
 valeurs_uniques = list(set(liste))
 
-cmap = cm.get_cmap('nipy_spectral', len(valeurs_uniques))  # Colormap
-
-
-def get_color(val):
-    # Convertir la valeur en un nombre entre 0 et 1
-    normalized = hashlib.md5(val.encode('utf-8')).hexdigest()
-    normalized = int(normalized, 16) / 16 ** len(normalized)
-
-    # Utiliser ce nombre pour obtenir une couleur
-    return matplotlib.colors.rgb2hex(cmap(normalized))
-
-
 # générer le dictionnaire couleur
-couleurs = {val: get_color(val) for val in valeurs_uniques}
+couleurs = {val: colors.to_hex(hsv_to_rgb(i/len(valeurs_uniques), 1, 1)) for i, val in enumerate(valeurs_uniques)}
 
 print(couleurs)
