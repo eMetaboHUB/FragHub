fa4d9af79d96df7b395dd944652adc19c82826db
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2024-02-27 15:12:23 +0100

    Refactor instruments normalization method
    Switched from csv to json for the instruments list data structure in the normalize_instruments_and_resolution.py script. This change simplifies the extraction and normalization process, by replacing the previous pandas methods with a json load method, improving overall code efficiency.

--- scripts/normalizer/normalize_instruments_and_resolution.py
+++ scripts/normalizer/normalize_instruments_and_resolution.py
@@ -1,21 +1,16 @@
-import pandas as pd
-import itertools
-import os
+import json
 import re
 
-global instruments_list
-instruments_list = pd.read_csv(os.path.abspath("../datas/instruments_catalogue.csv"), sep=";", encoding="UTF-8")
-instrument_types_list = instruments_list['INIT_INSTRUMENT_TYPE'].str.lower().fillna('')
-instruments_list = instruments_list['INIT_INSTRUMENT'].str.lower().fillna('')
-instruments_list = [' '.join(re.sub(r'[^\w\s]', ' ', ' '.join(pair)).split()) for pair in itertools.zip_longest(instruments_list, instrument_types_list, fillvalue='')]
 
-global instruments_dict
-instruments_dict = pd.read_csv(os.path.abspath("../datas/instruments_catalogue.csv"), sep=";", encoding="UTF-8")
-instruments_dict['INIT_INSTRUMENT'] = instruments_dict['INIT_INSTRUMENT'].str.lower().fillna('')
-instruments_dict['INIT_INSTRUMENT_TYPE'] = instruments_dict['INIT_INSTRUMENT_TYPE'].str.lower().fillna('')
-instruments_dict['INDEX'] = instruments_dict.apply(lambda row: ' '.join([''.join(sorted(word)) for word in re.sub(r'[^\w\s]', ' ', row['INIT_INSTRUMENT'] + " " + row['INIT_INSTRUMENT_TYPE']).split()]), axis=1)
-instruments_dict = instruments_dict.set_index('INDEX')
-instruments_dict = instruments_dict.T.to_dict('dict')
+global instrument_tree
+with open('../../datas/instruments_tree.json', 'r') as f:
+    instrument_tree = json.load(f)
+
+def clean_spectrum_instrument_info(metadata_dict):
+    instrument_infos = metadata_dict["INSTRUMENT"] + " " + metadata_dict["INSTRUMENTTYPE"]
+    instrument_infos = instrument_infos.lower()
+    instrument_infos = re.sub("-tof", "tof", instrument_infos)
+    instrument_infos = re.sub("q-", "q", instrument_infos)
 
 def normalize_instruments_and_resolution(metadata_dict):
     """
