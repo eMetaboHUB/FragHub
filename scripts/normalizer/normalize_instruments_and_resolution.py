1d32edfd676c84bab923ca3d3e6c43a2b890df59
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2024-02-28 14:03:58 +0100

    Refactor instrument normalization process
    Introduced several helper functions in the normalize_instruments_and_resolution.py script to efficiently locate and generate tree paths based on given instrument data. This new approach enhances the normalization process of instrument metadata. The main function, normalize_instruments_and_resolution, has been updated to utilize these new helper functions.

--- scripts/normalizer/normalize_instruments_and_resolution.py
+++ scripts/normalizer/normalize_instruments_and_resolution.py
@@ -66,13 +66,149 @@ def clean_spectrum_instrument_info(metadata_dict):
 
     return instrument_infos
 
+def search_for_brand(tree_path, instrument_infos):
+    """
+    Searches for a brand in instrument_infos and appends the found brand or 'unknown' to the tree_path.
+
+    :param tree_path: A list representing the path to the brand in the instrument_tree.
+    :param instrument_infos: A string containing information about the instrument.
+    :return: The updated tree_path list.
+    """
+    for key in instrument_tree.keys():
+        if f" {key} " in instrument_infos:
+            tree_path.append(key)
+            return tree_path
+
+    tree_path.append('unknown')
+    return tree_path
+
+def search_for_model(tree_path, instrument_infos):
+    """
+    Search for a model in the instrument tree based on the given tree path and instrument infos.
+
+    :param tree_path: The path to the model in the instrument tree.
+    :param instrument_infos: The instrument infos used to search for the model.
+    :return: The updated tree path with the found model or 'unknown' if not found.
+    """
+    for key in instrument_tree[tree_path[0]].keys():
+        if f" {key} " in instrument_infos:
+            tree_path.append(key)
+            return tree_path
+
+    tree_path.append('unknown')
+    return tree_path
+
+def search_for_spectrum_type(tree_path, instrument_infos):
+    """
+    Method to search for spectrum type in a given tree path and instrument infos.
+
+    :param tree_path: List of keys representing the tree path.
+    :param instrument_infos: String containing instrument information.
+    :return: Updated tree path with found spectrum type, or 'unknown' if not found.
+    """
+    for key in instrument_tree[tree_path[0]][tree_path[1]].keys():
+        if f" {key} " in instrument_infos:
+            tree_path.append(key)
+            return tree_path
+
+    tree_path.append('unknown')
+    return tree_path
+
+def search_for_instrument_type(tree_path, instrument_infos):
+    """
+    Search for instrument type in instrument_infos based on tree_path.
+
+    :param tree_path: list of indices representing the path in the instrument tree
+    :type tree_path: list[int]
+    :param instrument_infos: information about instruments
+    :type instrument_infos: str
+    :return: updated tree_path with the identified instrument type appended, otherwise 'unknown' if not found
+    :rtype: list[int]
+    """
+    for key in instrument_tree[tree_path[0]][tree_path[1]][tree_path[2]].keys():
+        if f" {key} " in instrument_infos:
+            tree_path.append(key)
+            return tree_path
+
+    tree_path.append('unknown')
+    return tree_path
+
+def search_for_ionisation(tree_path, instrument_infos):
+    """
+    :param tree_path: List of indices representing the path in the instrument tree.
+    :param instrument_infos: String containing information about instrument.
+    :return: Updated tree path.
+
+    This method searches for an ionisation by iterating through the keys in the instrument tree specified by the tree_path.
+    If a key is found in the instrument_infos string, it is appended to the tree_path and returned.
+    If no matching key is found, 'unknown' is appended to the tree_path and returned.
+    """
+    for key in instrument_tree[tree_path[0]][tree_path[1]][tree_path[2]][tree_path[3]].keys():
+        if f" {key} " in instrument_infos:
+            tree_path.append(key)
+            return tree_path
+
+    tree_path.append('unknown')
+    return tree_path
+
+def make_tree_path(instrument_infos):
+    """
+    :param instrument_infos: A list of dictionaries containing information about instruments
+    :return: A list representing the tree path based on the given instrument information
+
+    This method takes in a list of dictionaries containing information about instruments. It returns a list representing the tree path based on the given instrument information. The tree
+    * path is constructed by searching for specific attributes within the instrument information and appending them to the tree path list.
+
+    The method first initializes an empty list called tree_path. Then, it calls various search functions to populate the tree_path in the following order:
+
+    1. search_for_brand: searches for the brand attribute in the instrument information and appends it to the tree_path
+    2. search_for_model: searches for the model attribute in the instrument information and appends it to the tree_path
+    3. search_for_spectrum_type: searches for the spectrum type attribute in the instrument information and appends it to the tree_path
+    4. search_for_instrument_type: searches for the instrument type attribute in the instrument information and appends it to the tree_path
+    5. search_for_ionisation: searches for the ionisation attribute in the instrument information and appends it to the tree_path
+
+    Finally, the method returns the tree_path list.
+
+    Example usage:
+
+    instrument_info = [
+        {"brand": "Brand1", "model": "Model1", "spectrum_type": "Type1", "instrument_type": "TypeA", "ionisation": "IonA"},
+        {"brand": "Brand2", "model": "Model2", "spectrum_type": "Type2", "instrument_type": "TypeB", "ionisation": "IonB"},
+    ]
+
+    tree = make_tree_path(instrument_info)
+    print(tree)
+    # Output: ["Brand1", "Model1", "Type1", "TypeA", "IonA", "Brand2", "Model2", "Type2", "TypeB", "IonB"]
+    """
+    tree_path = []
+
+    tree_path = search_for_brand(tree_path, instrument_infos)
+    tree_path = search_for_model(tree_path, instrument_infos)
+    tree_path = search_for_spectrum_type(tree_path, instrument_infos)
+    tree_path = search_for_instrument_type(tree_path, instrument_infos)
+    tree_path = search_for_ionisation(tree_path, instrument_infos)
+
+    return tree_path
+
 def normalize_instruments_and_resolution(metadata_dict):
     """
-    Normalize the instrument metadata in the given dictionary.
+    :param metadata_dict: A dictionary containing metadata information.
+        - The dictionary should have the keys "INSTRUMENT", "INSTRUMENTTYPE", and "RESOLUTION".
+        - The values of these keys will be modified by this method.
+    :return: The modified metadata_dict dictionary.
 
-    :param metadata_dict: A dictionary containing the instrument metadata.
-    :return: The normalized instrument metadata dictionary.
+    This method normalizes the instrument and resolution information in the given metadata_dict. It retrieves the instrument information from the metadata_dict, cleans it, and updates the
+    * relevant keys in the metadata_dict with the cleaned values.
     """
-    clean_spectrum_instrument_info(metadata_dict)
+    instrument_infos = clean_spectrum_instrument_info(metadata_dict)
+
+    tree_path = make_tree_path(instrument_infos)
+
+    solution = instrument_tree[tree_path[0]][tree_path[1]][tree_path[2]][tree_path[3]][tree_path[4]]["SOLUTION"]
+    solution = solution.split(',')
+
+    metadata_dict["INSTRUMENT"] = solution[0].strip()
+    metadata_dict["INSTRUMENTTYPE"] = solution[1].strip()
+    metadata_dict["RESOLUTION"] = solution[2].strip()
 
     return metadata_dict
