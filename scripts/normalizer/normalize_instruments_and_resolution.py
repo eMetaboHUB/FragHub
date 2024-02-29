dde5f91a98e2153f270ab79fdfd0c9af644c335f
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2024-02-29 09:25:32 +0100

    Enhance debugging and update instrument normalization functions
    Improved debugging by adding print statements for a specific 'FRAGHUBID' in the instrument normalization functions. Also, two functions were updated: 'search_for_ionisation' now includes a 'mode' parameter, and 'make_tree_path' now considers 'metadata_dict'. Now, if a specific FRAGHUBID is found, specific debugging information is printed while creating the tree path and searching for ionisation.

--- scripts/normalizer/normalize_instruments_and_resolution.py
+++ scripts/normalizer/normalize_instruments_and_resolution.py
@@ -145,7 +145,7 @@ def search_for_instrument_type(tree_path, instrument_infos):
     except:
         return None
 
-def search_for_ionisation(tree_path, instrument_infos):
+def search_for_ionisation(tree_path, instrument_infos, mode):
     """
     :param tree_path: List of indices representing the path in the instrument tree.
     :param instrument_infos: String containing information about instrument.
@@ -156,6 +156,8 @@ def search_for_ionisation(tree_path, instrument_infos):
     If no matching key is found, 'unknown' is appended to the tree_path and returned.
     """
     try:
+        if mode:
+            print(instrument_tree[tree_path[0]][tree_path[1]][tree_path[2]][tree_path[3]])
         for key in instrument_tree[tree_path[0]][tree_path[1]][tree_path[2]][tree_path[3]].keys():
             if re.search(rf"(\b)?{key}(\b)?",instrument_infos):
                 tree_path.append(key)
@@ -166,7 +168,7 @@ def search_for_ionisation(tree_path, instrument_infos):
     except:
         return None
 
-def make_tree_path(instrument_infos):
+def make_tree_path(instrument_infos, metadata_dict):
     """
     :param instrument_infos: A list of dictionaries containing information about instruments
     :return: A list representing the tree path based on the given instrument information
@@ -198,18 +200,32 @@ def make_tree_path(instrument_infos):
     tree_path = []
 
     tree_path = search_for_brand(tree_path, instrument_infos)
+    if metadata_dict['FRAGHUBID'] == 'cf6a4cfb6680a0fda8cc69d29125b50e01dafc4a8b5060cba8cfd3d445296268':
+        print(tree_path)
     if not tree_path:
         return None
     tree_path = search_for_model(tree_path, instrument_infos)
+    if metadata_dict['FRAGHUBID'] == 'cf6a4cfb6680a0fda8cc69d29125b50e01dafc4a8b5060cba8cfd3d445296268':
+        print(tree_path)
     if not tree_path:
         return None
     tree_path = search_for_spectrum_type(tree_path, instrument_infos)
+    if metadata_dict['FRAGHUBID'] == 'cf6a4cfb6680a0fda8cc69d29125b50e01dafc4a8b5060cba8cfd3d445296268':
+        print(tree_path)
     if not tree_path:
         return None
     tree_path = search_for_instrument_type(tree_path, instrument_infos)
+    if metadata_dict['FRAGHUBID'] == 'cf6a4cfb6680a0fda8cc69d29125b50e01dafc4a8b5060cba8cfd3d445296268':
+        print(tree_path)
     if not tree_path:
         return None
-    tree_path = search_for_ionisation(tree_path, instrument_infos)
+    if metadata_dict['FRAGHUBID'] == 'cf6a4cfb6680a0fda8cc69d29125b50e01dafc4a8b5060cba8cfd3d445296268':
+        tree_path = search_for_ionisation(tree_path, instrument_infos, True)
+    else:
+        tree_path = search_for_ionisation(tree_path, instrument_infos, False)
+    if metadata_dict['FRAGHUBID'] == 'cf6a4cfb6680a0fda8cc69d29125b50e01dafc4a8b5060cba8cfd3d445296268':
+        print(instrument_infos)
+        print(tree_path)
     if not tree_path:
         return None
 
@@ -227,9 +243,9 @@ def normalize_instruments_and_resolution(metadata_dict):
     """
     instrument_infos = clean_spectrum_instrument_info(metadata_dict)
 
-    # instrument_infos = f". {instrument_infos} ."
+    instrument_infos = f". {instrument_infos} ."
 
-    tree_path = make_tree_path(instrument_infos)
+    tree_path = make_tree_path(instrument_infos,metadata_dict)
     if not tree_path:
         return metadata_dict
 
