40a8229eeec2c4a539e7bb4ba54f1bfc1b1d0c16
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2024-04-11 16:22:22 +0200

    Update normalization function in normalize_intensity.py
    The normalization function checks if the maximum intensity value in a numpy array is non-zero. If so, it normalizes all intensity values by dividing them by this maximum value. If the maximum intensity is zero, the function now returns an empty numpy array.

--- scripts/peaks_filters/normalize_intensity.py
+++ scripts/peaks_filters/normalize_intensity.py
@@ -4,16 +4,24 @@ def normalize_intensity(peak_array):
     """
     Normalize (rescale) the intensity values of a numpy array.
 
+    This function checks if the maximum value of intensity (2nd column of the array) is non-zero,
+    if it is then the function divides all the intensity values by the maximum intensity value
+    to normalize them. If the maximum intensity is zero, the function returns an empty numpy array.
+
     :param peak_array: A numpy array containing peak data.
                        The array is assumed to have two columns,
                        with the second column containing the intensities.
     :type peak_array: numpy.ndarray
     :return: The input numpy array with normalized intensity values.
+             If the max intensity is zero, an empty array is returned.
     :rtype: numpy.ndarray
     """
+    # Check if the maximum intensity value is not zero
     if peak_array[:, 1].max() != 0:
+        # If so, normalize all intensity values by the maximum value
         peak_array[:, 1] = peak_array[:, 1] / peak_array[:, 1].max()
-
+        # Return the array with normalized intensity
         return peak_array
 
+    # If maximum intensity is zero, return an empty array
     return np.array([])
