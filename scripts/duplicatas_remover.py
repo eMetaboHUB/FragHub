537ecc0c80f4b7ece191405bd8f3877618c0bf81
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2024-04-11 16:25:58 +0200

    Refactor duplicate removal and progress tracking
    The code for removing duplicates and updating the progress bar in various dataframes has been refactored for better understanding. Comments have been added to explain the steps more clearly while redundant comments have been removed. The section labels like `# ========================================================================= POS_LC =========================================================================` have been simplified.

--- scripts/duplicatas_remover.py
+++ scripts/duplicatas_remover.py
@@ -6,7 +6,6 @@ import os
 def removing_duplicates(dataframe, name, mode, update, first_run, profile_name):
     """
     Remove duplicates from a dataframe and update the progress bar.
-
     :param dataframe: The input dataframe.
     :param name: The name of the dataframe.
     :param mode: The mode of the dataframe.
@@ -15,31 +14,37 @@ def removing_duplicates(dataframe, name, mode, update, first_run, profile_name):
     :param profile_name: The profile name.
     :return: The dataframe after removing duplicates.
     """
+    # Check if the dataframe should be updated and not the first run
     if update and not first_run:
+        # Create the complete file path
         filename = os.path.abspath(f"../OUTPUT/{profile_name}/CSV/{mode}/{name}.csv")
+        # Check if a file exists at the path, if so read it into a dataframe, else create an empty dataframe
         if os.path.exists(filename):
             csv_dataframe = pd.read_csv(filename, sep=";", quotechar='"', encoding="UTF-8")
         else:
             csv_dataframe = pd.DataFrame()
-
+        # Concatenate the input and loaded dataframe
         dataframe = pd.concat([dataframe, csv_dataframe])
-
+    # Calculate the total count of rows in the dataframe for progress tracking
     total_rows = len(dataframe)
+    # Create a progress bar with the number of rows in the dataframe as the total
     t = tqdm(total=len(dataframe), desc="{:>70}".format(name), colour="green", unit=" row")
 
-    # Delete duplicates and update progress bar
+    # Remove any duplicate rows from the dataframe based on 'INCHIKEY' and 'PEAKS_LIST' columns
     dataframe = dataframe.loc[~dataframe.duplicated(subset=['INCHIKEY', 'PEAKS_LIST'])]
+
+    # Update the progress bar with the original total number of rows
     t.update(total_rows)
 
-    # Close progress bar
+    # Close the progress bar
     t.close()
 
+    # Return the dataframe with duplicates removed
     return dataframe
 
 def remove_duplicatas(POS_LC_df, POS_LC_In_Silico_df, POS_GC_df, POS_GC_In_Silico_df, NEG_LC_df, NEG_LC_In_Silico_df, NEG_GC_df, NEG_GC_In_Silico_df, first_run, profile_name, update=False):
     """
     Remove duplicates from the given dataframes.
-
     :param POS_LC_df: The dataframe for positive LC data.
     :param POS_LC_In_Silico_df: The dataframe for positive LC In Silico data.
     :param POS_GC_df: The dataframe for positive GC data.
@@ -53,29 +58,29 @@ def remove_duplicatas(POS_LC_df, POS_LC_In_Silico_df, POS_GC_df, POS_GC_In_Silic
     :param update: A boolean value indicating if the dataframes should be updated. Default is False.
     :return: The updated dataframes for POS_LC, POS_LC_In_Silico, POS_GC, POS_GC_In_Silico, NEG_LC, NEG_LC_In_Silico, NEG_GC, and NEG_GC_In_Silico.
     """
-    # ========================================================================= POS_LC =========================================================================
+    # Remove duplicates from the POS_LC dataframe
     POS_LC_df = removing_duplicates(POS_LC_df, "POS_LC", "POS", update, first_run, profile_name)
 
-    # ========================================================================= POS_LC_In_Silico =========================================================================
+    # Remove duplicates from the POS_LC_In_Silico dataframe
     POS_LC_In_Silico_df = removing_duplicates(POS_LC_In_Silico_df, "POS_LC_In_Silico", "POS", update, first_run, profile_name)
 
-    # ========================================================================= POS_GC =========================================================================
+    # Remove duplicates from the POS_GC dataframe
     POS_GC_df = removing_duplicates(POS_GC_df, "POS_GC", "POS", update, first_run, profile_name)
 
-    # ========================================================================= POS_GC_In_Silico =========================================================================
+    # Remove duplicates from the POS_GC_In_Silico dataframe
     POS_GC_In_Silico_df = removing_duplicates(POS_GC_In_Silico_df, "POS_GC_In_Silico", "POS", update, first_run, profile_name)
 
-    # ========================================================================= NEG_LC =========================================================================
+    # Remove duplicates from the NEG_LC dataframe
     NEG_LC_df = removing_duplicates(NEG_LC_df, "NEG_LC", "NEG", update, first_run, profile_name)
 
-    # ========================================================================= NEG_LC_In_Silico =========================================================================
+    # Remove duplicates from the NEG_LC_In_Silico dataframe
     NEG_LC_In_Silico_df = removing_duplicates(NEG_LC_In_Silico_df, "NEG_LC_In_Silico", "NEG", update, first_run, profile_name)
 
-    # ========================================================================= NEG_GC =========================================================================
+    # Remove duplicates from the NEG_GC dataframe
     NEG_GC_df = removing_duplicates(NEG_GC_df, "NEG_GC", "NEG", update, first_run, profile_name)
 
-    # ========================================================================= NEG_GC_In_Silico =========================================================================
+    # Remove duplicates from the NEG_GC_In_Silico dataframe
     NEG_GC_In_Silico_df = removing_duplicates(NEG_GC_In_Silico_df, "NEG_GC_In_Silico", "NEG", update, first_run, profile_name)
 
-
-    return POS_LC_df,POS_LC_In_Silico_df,POS_GC_df,POS_GC_In_Silico_df,NEG_LC_df,NEG_LC_In_Silico_df,NEG_GC_df,NEG_GC_In_Silico_df
+    # Return the updated dataframes
+    return POS_LC_df, POS_LC_In_Silico_df, POS_GC_df, POS_GC_In_Silico_df, NEG_LC_df, NEG_LC_In_Silico_df, NEG_GC_df, NEG_GC_In_Silico_df
