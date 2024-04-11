52aa2d3b3c5d7df953f14f896e48e0d686b938d9
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2024-04-11 15:18:57 +0200

    Refactor and add comments in JSON conversion script
    The script 'json_to_json.py' that converts JSON objects has been refactored to improve code readability. Comments have been added to explain the utility and the purpose of various sections in the code. The changes improve the efficiency and understandability of spectrum data extraction and processing in the script.

--- scripts/convertors/json_to_json.py
+++ scripts/convertors/json_to_json.py
@@ -15,8 +15,12 @@ def parse_MoNA_peak_list(peak_list_string):
     :return: A list of peaks, where each peak is represented as a list containing the m/z value and intensity.
     :rtype: list[list[float, float]]
     """
+    # A regular expression pattern is used here to find all matches within the peak list string. Each match
+    # is a peak represented in JSON format.
     peaks = re.findall(peak_list_json_to_json_pattern, peak_list_string)
 
+    # Here, we convert each peak (m/z and intensity) into float type and store them in a list.
+    # The list of these peaks is returned by the function.
     return [[float(mz), float(intensity)] for mz, intensity in peaks]
 
 def convert_MoNA_json(json_dict):
@@ -29,13 +33,15 @@ def convert_MoNA_json(json_dict):
     :return: A dictionary containing the converted information from the MoNA entry.
 
     """
-    dict_final = {}
+    dict_final = {}  # Initialize the final output dictionary
 
+    # Try to fetch compound name from the json_dict, if not available add an empty string
     try:
         dict_final["compound_name"] = json_dict["compound"][0]["names"][0]["name"]
     except:
         dict_final["compound_name"] = ''
 
+    # Try to fetch molecular formula, SMILES, InChI, InChIKey from the json_dict, if not available pass
     try:
         for i in range(len(json_dict["compound"][0]["metaData"])):
             if json_dict["compound"][0]["metaData"][i]["name"] in ["molecular formula", "SMILES", "InChI", "InChIKey"] and not json_dict["compound"][0]["metaData"][i]["computed"]:
@@ -43,11 +49,13 @@ def convert_MoNA_json(json_dict):
     except:
         pass
 
+    # Try to fetch spectrum_id from the json_dict, if not available pass
     try:
         dict_final["spectrum_id"] = json_dict["id"]
     except:
         pass
 
+    # Try to fetch different metadata from the json_dict, if not available pass
     try:
         for i in range(len(json_dict["metaData"])):
             if json_dict["metaData"][i]["name"] in ["instrument", "instrument type", "ms level", "ionization", "retention time", "ionization mode", "precursor type", "collision energy", "precursor m/z"]:
@@ -55,11 +63,13 @@ def convert_MoNA_json(json_dict):
     except:
         pass
 
+    # Try to fetch filename from the json_dict, if not available pass
     try:
         dict_final["filename"] = json_dict["filename"]
     except:
         pass
 
+    # Try to fetch peaks from the json_dict and parse them, if not available pass
     try:
         peak_list_string = json_dict["spectrum"]
         dict_final["peaks"] = parse_MoNA_peak_list(peak_list_string)
@@ -72,62 +82,92 @@ def parse_others_json_peak_list(peak_list):
     """
     Parses a JSON peak list and returns a list of peaks.
 
+    This function expects a peak list in JSON format. In the first step, it uses a predefined regular expression
+    pattern `peak_list_json_to_json_pattern` (which is not shown in this code) to find all peaks in the JSON data.
+    The `re.findall` function returns a list of tuples where each tuple contains the mz and the intensity values
+    as strings. Next, it loops over the list using a list comprehension. For each tuple, it converts the mz and the
+    intensity values to float and groups them as a list. The function returns a list of these list groups. Each list
+    group represents a peak with the mz and intensity values.
+
     :param peak_list: A JSON string representing the peak list.
     :return: A list of peak tuples where each tuple contains the mz (float) and intensity (float) values.
     """
+
+    # Use a regex pattern to find all peaks in the JSON string
     peak_list = re.findall(peak_list_json_to_json_pattern, peak_list)
 
+    # convert each peak's mz and intensity values to float and group them in a list
+    # return a list of these peak lists
     return [[float(mz), float(intensity)] for mz, intensity in peak_list]
 
 def json_to_json(json_dict):
     """
     :param json_dict: A dictionary representing a JSON object.
     :return: The converted JSON object.
-
-    This method takes a JSON object represented as a dictionary and checks if it contains the keys "compound", "id", "metaData", and "spectrum". If all of these keys are present, the method
-    * calls the function "convert_MoNA_json" to convert the JSON object. Otherwise, it returns the original JSON object.
-
-    Note: The "convert_MoNA_json" function is not implemented in this code, so you need to define it separately before using this method.
+    Check if the JSON object represented by the dictionary contains all the required keys.
+    If it does, transform it by calling 'convert_MoNA_json' and 'convert_keys'.
+    If any peak list keys are present, transform the corresponding values and return the modified object.
+    If none of these conditions are met, return None.
     """
 
+    # Define keys needed for conversion
     keys_to_check = ["compound", "id", "metaData", "spectrum", "filename"]
+
+    # Define possible keys for peak values
     peak_list_keys = ["spectrum", "peaks_json", "peaks"]
 
+    # If the JSON dictionary has all keys_to_check,
+    # Convert it using convert_MoNA_json function and convert_keys
     if all(key in json_dict for key in keys_to_check):
-        json_dict = convert_MoNA_json(json_dict)
-        json_dict = convert_keys(json_dict)
+        json_dict = convert_MoNA_json(json_dict)  # Transform JSON object
+        json_dict = convert_keys(json_dict)  # Standardize keys
         return json_dict
+
+    # Else if it has a peak from peak_list_keys,
+    # parse the peak values and convert keys
     else:
         for key in peak_list_keys:
             if key in json_dict:
-                json_dict[key] = parse_others_json_peak_list(str(json_dict[key]))
-                json_dict = convert_keys(json_dict)
+                json_dict[key] = parse_others_json_peak_list(str(json_dict[key]))  # Transform peak values
+                json_dict = convert_keys(json_dict)  # Standardize keys
                 return json_dict
+
+    # If there's neither required keys nor peak keys, return None
     return None
 
 def json_to_json_processing(FINAL_JSON):
     """
     Converts a list of JSON objects into processed JSON data.
-
     :param FINAL_JSON: A list of JSON objects to be processed.
     :return: A list of processed JSON data.
-
     This method takes a list of JSON objects and processes them using the 'json_to_json' method. The list is divided into chunks of size 'chunk_size'. Each chunk is processed concurrently
     * using a ThreadPoolExecutor, and the results are collected and returned as a list of processed JSON data.
     """
+    # Set the predefined chunk size
     chunk_size = 5000
+    # Initialize an empty list for storing the results
     final = []
+    # Initialize a progress bar for providing progress updates
     progress_bar = tqdm(total=len(FINAL_JSON), unit=" spectrums", colour="green", desc="{:>70}".format("converting JSON spectrums"))
 
-    # Dividing the spectrum list into chunks
+    # Start of JSON processing logic
+    # This loop repeatedly divides the list of JSON data into chunks of prespecified size
     for i in range(0, len(FINAL_JSON), chunk_size):
+        # Each JSON chunk within the larger list is being processed concurrently
+        # The ThreadPoolExecutor is used for achieving parallel execution
         with concurrent.futures.ThreadPoolExecutor() as executor:
+            # Get the current chunk to be processed
             chunk = FINAL_JSON[i:i + chunk_size]
+            # Process the current chunk using the 'json_to_json' function
             results = list(executor.map(json_to_json, chunk))
+            # Update the progress bar based on chunk size
             progress_bar.update(len(chunk))
-
+        # Extend the final list with results after checking whether they are None
+        # Filter out any None results before adding to final list
         final.extend([res for res in results if res is not None])
 
+    # Close the progress bar after all processing is complete
     progress_bar.close()
 
+    # Return the final list of processed JSON data
     return final
