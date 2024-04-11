57049cb0aec8eb4b3147d62f020b18dc772bc39d
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2024-04-11 16:07:36 +0200

    Refactor normalize_ionmode function for clarity
    The update simplifies and clarifies the normalize_ionmode function in the "scripts/normalizer" directory. The changes mainly involve adding comments to explain what each line of code does, making the function easier to understand for other developers. This includes elucidating the handling of ion mode patterns and their subsequent normalizing process.

--- scripts/normalizer/normalize_ionmode.py
+++ scripts/normalizer/normalize_ionmode.py
@@ -12,13 +12,19 @@ def normalize_ionmode(metadata_dict):
         It should have the key "IONMODE" to represent the ionization mode.
     :return: The updated metadata dictionary with the "IONMODE" value normalized to either "positive" or "negative".
     """
+
+    # get the value of "IONMODE" from the metadata dictionary
     ionmode = metadata_dict["IONMODE"]
 
+    # If the pattern for positive ion mode is found in ion mode, change it to normalized form "positive"
     if re.search(ionmode_pos_pattern, ionmode):
         ionmode = "positive"
+    # If the pattern for negative ion mode is found in ion mode, change it to normalized form "negative"
     elif re.search(ionmode_neg_pattern, ionmode):
         ionmode = "negative"
 
+    # update the "IONMODE" key in metadata dictionary with normalized ion mode value
     metadata_dict["IONMODE"] = ionmode
 
+    # return the updated metadata dictionary
     return metadata_dict
