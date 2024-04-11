c9d15b16e7ba43c64b0b589026d0e5f2942647e2
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2024-04-11 16:40:24 +0200

    Improve code readability and add comments
    The update primarily improves the readability of the code by adding line breaks and spaces. It also includes more descriptive comments on key sections of the code to better understand what the functions are doing, such as when initializing profiles, resetting updates, removing files, and converting parameters.

--- scripts/set_parameters.py
+++ scripts/set_parameters.py
@@ -18,25 +18,30 @@ func_names = [
 def on_done_button_clicked():
     """
     Closes the root window and converts the values in the parameters_dict dictionary to floats.
+    This function is triggered when the 'Done' button of the GUI is clicked.
 
     :return: None
     """
+
+    # Loop through a defined list of keys (pertaining to various functionalities) and check if they exist in the parameters dictionary
     for key in [
-        'reset_updates',
-        'csv',
-        'msp',
-        'json'
-    ] + func_names + [
-        'check_minimum_peak_requiered_n_peaks',
-        'reduce_peak_list_max_peaks',
-        'keep_mz_in_range_from_mz',
-        'keep_mz_in_range_to_mz',
-        'check_minimum_of_high_peaks_requiered_intensity_percent',
-        'check_minimum_of_high_peaks_requiered_no_peaks'
-    ]:
+                   'reset_updates',
+                   'csv',
+                   'msp',
+                   'json'
+               ] + func_names + [
+                   'check_minimum_peak_requiered_n_peaks',
+                   'reduce_peak_list_max_peaks',
+                   'keep_mz_in_range_from_mz',
+                   'keep_mz_in_range_to_mz',
+                   'check_minimum_of_high_peaks_requiered_intensity_percent',
+                   'check_minimum_of_high_peaks_requiered_no_peaks'
+               ]:
+        # If the key exists in the dictionary, retrieve its value and convert it to a float
         if key in parameters_dict:
             parameters_dict[key] = float(parameters_dict[key].get())
 
+    # Close the root window after all the values are converted and saved back into the dictionary
     root.destroy()
 
 def build_window():
@@ -166,30 +171,32 @@ def remove_files(directory):
     """
     Removes all files (except .gitkeep) in the given directory and its subdirectories.
     """
-    for filename in os.listdir(directory):
-        if filename == '.gitkeep':
-            continue  # skip .gitkeep files
 
-        file_path = os.path.join(directory, filename)
-        if os.path.isfile(file_path):
+    for filename in os.listdir(directory):  # iterate through each file in the directory
+        if filename == '.gitkeep':
+            continue  # skip .gitkeep files as we don't want to remove these
+        file_path = os.path.join(directory, filename)  # create a complete filepath
+
+        if os.path.isfile(file_path):  # if the path is a file
             os.remove(file_path)  # remove the file
-        elif os.path.isdir(file_path):
-            remove_files(file_path)  # call this function again with the subdirectory
+        elif os.path.isdir(file_path):  # if the path is a directory
+            remove_files(file_path)  # call this function recursively to remove files in subdirectory
 
 def reset_updates(profile_name):
     """
     Resets the updates by deleting the contents of the updates.json file and removing any existing output files.
     """
-    json_update_path = rf"../datas/updates/{profile_name}.json"
-    ouput_path = rf"../OUTPUT/{profile_name}"
 
-    # Reset the json file - Writing empty json object
+    json_update_path = rf"../datas/updates/{profile_name}.json"  # path to the relevant update.json file
+    ouput_path = rf"../OUTPUT/{profile_name}"  # path to the relevant output directory
+
+    # Reset the json file - Writing an empty json object to the file effectively clears it
     with open(json_update_path, 'w') as f:
         json.dump({}, f)
 
     # Remove output files
-    if os.path.exists(ouput_path):
-        remove_files(ouput_path)
+    if os.path.exists(ouput_path):  # if the output directory exists
+        remove_files(ouput_path)  # call the remove_files function to remove all files in the directory
 
 def init_profile(profile_name):
     """
@@ -198,30 +205,38 @@ def init_profile(profile_name):
     :param profile_name: The name of the profile to initialize.
     :return: None
     """
-    # Get the file path
+
+    # Path to the file where updates will be stored
     updates_file_path = os.path.join("../datas/updates", profile_name + ".json")
+
+    # Directory where outputs will be stored for the profile
     output_directory = os.path.join("../OUTPUT", profile_name)
 
+    # List of main directories to be created under the output directory
     main_directories = ['CSV', 'JSON', 'MSP']
+
+    # List of subdirectories to be created under each main directory
     sub_directories = ['NEG', 'POS']
 
-    # Check if file doesn't exist
+    # Checking if the updates file already exists
     if not os.path.isfile(updates_file_path):
-        # Create the file
+        # Creating the updates file since it doesn't exist
         with open(updates_file_path, 'w') as fp:
-            # Write an empty JSON object to the file
+            # Write an empty JSON object to the file as initial content
             json.dump({}, fp)
 
+    # Checking if the output directory for the profile already exists
     if not os.path.isdir(output_directory):
-        # Create the directory
+        # Creating the output directory since it doesn't exist
         os.makedirs(output_directory)
 
-    # Loop through each main directory
+    # For each directory in main_directories list...
     for main_dir in main_directories:
-        # Loop through each sub directory
+        # ...and for each subdirectory in sub_directories list...
         for sub_dir in sub_directories:
-            # Get the sub directory path
+            # ...create a full path to the subdirectory
             dir_path = os.path.join(output_directory, main_dir, sub_dir)
-            # If the directory doesn't exist, create it
+            # If the directory path doesn't exist...
             if not os.path.isdir(dir_path):
+                # ...create the subdirectory
                 os.makedirs(dir_path)
