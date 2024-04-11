3289f24ca468bb6cbeec930d5ecb404fba0b6ba2
axel.dablanc <axel.dablanc@univ-tlse3.fr>
2024-04-11 16:23:50 +0200

    Refine remove_peak_above_precursormz function documentation
    The detail of revise focuses on function `remove_peak_above_precursormz`, in the `peaks_filters` script. The function documentation is further clarified with more explicit details regarding the parameters and the functionality of the filtering criterion. Also, a bit more error handling is added for when the input `precursormz` is not a float.

--- scripts/peaks_filters/remove_peak_above_precursormz.py
+++ scripts/peaks_filters/remove_peak_above_precursormz.py
@@ -1,12 +1,20 @@
 
 def remove_peak_above_precursormz(peak_array, precursormz):
     """
-    :param peak_array: numpy array containing peak data
-    :param precursormz: float representing the precursor m/z value
-    :return: filtered numpy array with peaks below the specified precursor m/z value + 5 Da
-    """
-    if isinstance(precursormz, float):
-        peak_array = peak_array[peak_array[:,0] < precursormz + 5.0]  # Removing peaks with mz > precursormz + 5 Da
-        return peak_array
+    Remove peaks that are above a specified precursor m/z value.
 
-    return peak_array
+    :param peak_array: A numpy array containing peak data. It's expected to include the m/z (mass-to-charge ratio)
+                       values in the first column and corresponding intensities in the second column.
+
+    :param precursormz: A float representing the precursor m/z value. Peaks with m/z values greater than
+                        (precursormz + 5 Da) are considered as noise and removed from the peak array.
+
+    :return: The filtered numpy array that only includes peaks with m/z values below the (precursor m/z + 5 Da).
+    """
+    # check if the input precursormz is a floating-point number
+    if isinstance(precursormz, float):
+        # filter the array to only include peaks with m/z less than (precursor m/z + 5 Da)
+        peak_array = peak_array[peak_array[:, 0] < precursormz + 5.0]  # Filtering criterion
+        return peak_array  # Return the filtered array
+
+    return peak_array  # If precursormz is not a float, return the original peak array without any filtering
