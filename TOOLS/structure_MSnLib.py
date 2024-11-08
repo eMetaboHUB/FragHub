939dfc55095d3dcbe53df82319217e840b060705
Axel.Dablanc <axel.dablanc@univ-tlse3.fr>
2024-11-08 15:25:27 +0100

    Fix column renaming and transform peaks format
    Renamed the column 'MS2Peaks' to 'peaks_json' and transformed 'peaks_json' values from string format to matrices. This ensures consistent data representation and facilitates further processing.

--- TOOLS/structure_MSnLib.py
+++ TOOLS/structure_MSnLib.py
@@ -14,7 +14,7 @@ fichiers_txt = [f for f in fichiers if f.endswith('.txt')]
 # Liste pour stocker tous les DataFrames
 dataframes = []
 
-# Lire chaque fichier .txt avec pandas et ajouter au liste des DataFrames
+# Lire chaque fichier .txt avec pandas et ajouter à la liste des DataFrames
 for fichier in fichiers_txt:
     chemin_fichier = os.path.join(dossier, fichier)
     df = pd.read_csv(chemin_fichier, sep=';')
@@ -24,7 +24,19 @@ for fichier in fichiers_txt:
 df_concat = pd.concat(dataframes, ignore_index=True)
 
 # Renommer la colonne 'MS2Peaks' en 'peaks'
-df_concat.rename(columns={'MS2Peaks': 'peaks'}, inplace=True)
+df_concat.rename(columns={'MS2Peaks': 'peaks_json'}, inplace=True)
+
+
+# Transformer chaque valeur de 'peaks' en matrice
+def transform_peaks(peaks_str):
+    peak_list = []
+    for peak in peaks_str.strip().split('\n'):
+        mass, intensity = map(float, peak.split('\t'))
+        peak_list.append([mass, intensity])
+    return peak_list
+
+
+df_concat['peaks_json'] = df_concat['peaks_json'].apply(transform_peaks)
 
 # Convertir en dictionnaire format records
 dict_records = df_concat.to_dict(orient='records')
