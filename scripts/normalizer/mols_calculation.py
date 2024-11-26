59e2838f7014f243fb6f337236190f2388e8f302
Axel.Dablanc <axel.dablanc@univ-tlse3.fr>
2024-11-26 15:39:08 +0100

    Parallelize CSV loading and merging in mols_calculation.py
    Added functionality to load and merge CSV files in parallel using ThreadPoolExecutor for improved performance. This reduces the overall loading time for large datasets stored in the '../datas/pubchem_datas' folder.

--- scripts/normalizer/mols_calculation.py
+++ scripts/normalizer/mols_calculation.py
@@ -1,11 +1,35 @@
 from rdkit.Chem.rdMolDescriptors import CalcMolFormula
 from rdkit.Chem.Descriptors import ExactMolWt, MolWt
+from concurrent.futures import ThreadPoolExecutor
 from rdkit import RDLogger, Chem
 from tqdm import tqdm
 import pandas as pd
 import re
+import os
 
 RDLogger.DisableLog('rdApp.*') # Disable rdkit log (warning) messages
+
+# Dossier contenant les fichiers CSV
+folder_path = '../datas/pubchem_datas'
+# Liste pour stocker chaque DataFrame
+all_dfs = []
+# Fonction pour lire un fichier CSV
+def read_csv(file_path):
+    return pd.read_csv(file_path, sep=';', quotechar='"', encoding='utf-8')
+
+# Utiliser ThreadPoolExecutor pour lire les fichiers en parallèle
+with ThreadPoolExecutor() as executor:
+    futures = []
+    for file_name in os.listdir(folder_path):
+        if file_name.endswith('.csv'):
+            file_path = os.path.join(folder_path, file_name)
+            futures.append(executor.submit(read_csv, file_path))
+    # Récupérer les résultats des futures
+    for future in futures:
+        all_dfs.append(future.result())
+
+# Concaténer tous les DataFrames
+pubchem_datas = pd.concat(all_dfs, ignore_index=True)
 
 inchikey_pattern = re.compile(r"([A-Z]{14}-[A-Z]{10}-[NO])|([A-Z]{14})", flags=re.IGNORECASE) # Match inchikey or short inchikey
 indigo_smiles_correction_pattern = re.compile(r"\|[\s\S]*")
