8785c08bd4e07caf6281a19ae16f0e5bfbb83f13
Axel.Dablanc <axel.dablanc@univ-tlse3.fr>
2024-12-02 14:57:25 +0100

    Add function to correct and convert InChI and SMILES
    Introduce a new function `correct_and_convert_inchi_smiles` to handle normalization and conversion of InChI and SMILES data. This function ensures valid chemical formats by checking and converting InChI data to SMILES if needed, and vice versa, cleaning invalid entries. Additionally, apply this function to concatenate DataFrame for standardized molecular calculations.

--- scripts/normalizer/mol_calculation_v2.py
+++ scripts/normalizer/mol_calculation_v2.py
@@ -30,3 +30,32 @@ with ThreadPoolExecutor() as executor:
 
 # Concaténer tous les DataFrames
 pubchem_datas = pd.concat(all_dfs, ignore_index=True)
+
+inchikey_pattern = re.compile(r"([A-Z]{14}-[A-Z]{10}-[NO])|([A-Z]{14})", flags=re.IGNORECASE) # Match inchikey or short inchikey
+indigo_smiles_correction_pattern = re.compile(r"\|[\s\S]*")
+
+def correct_and_convert_inchi_smiles(row):
+    # Si INCHIKEY est présent et correspond au pattern, ne pas faire d'autres traitements
+    if pd.notna(row['INCHIKEY']) and inchikey_pattern.match(row['INCHIKEY']):
+        return row
+
+    if pd.notna(row['INCHI']):
+        if 'InChI=' not in row['INCHI']:
+            cleaned_value = re.sub(indigo_smiles_correction_pattern, "", row['INCHI'])
+            if Chem.MolFromSmiles(cleaned_value):
+                row['INCHI'] = Chem.MolToSmiles(Chem.MolFromSmiles(cleaned_value))
+            else:
+                row['INCHI'] = ''  # Nettoyer si ce n'est ni un INCHI valide, ni un SMILES valide
+
+    if pd.notna(row['SMILES']):
+        if 'InChI=' in row['SMILES']:
+            if Chem.MolFromInchi(row['SMILES']):
+                row['SMILES'] = Chem.MolToInchi(Chem.MolFromInchi(row['SMILES']))
+            else:
+                row['SMILES'] = ''  # Nettoyer si ce n'est pas un SMILES valide
+
+    return row
+
+def mols_derivation_and_calculation(CONCATENATE_DF):
+    # Application de la fonction à chaque ligne du DataFrame
+    CONCATENATE_DF = CONCATENATE_DF.apply(correct_and_convert_inchi_smiles, axis=1)
