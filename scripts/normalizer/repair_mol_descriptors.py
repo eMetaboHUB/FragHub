9953b8d16c4a60cd91af1eb836b0d2f8f83a34d9
Axel.Dablanc <axel.dablanc@univ-tlse3.fr>
2024-12-05 11:19:16 +0100

    Enhance InChI and SMILES parsing logic
    Improve InChI pattern matching to ensure more comprehensive captures. Reinforce SMILES extraction with additional conditions to handle special characters effectively. Rearrange the call to the repair function to maintain updated InChI in the metadata dictionary.

--- scripts/normalizer/repair_mol_descriptors.py
+++ scripts/normalizer/repair_mol_descriptors.py
@@ -4,7 +4,7 @@ global smiles_pattern
 smiles_pattern = re.compile(r"\b([^J][0-9BCOHNSOPrIFla@+\-\[\]\(\)\\\/%=#$]{6,})\b", flags=re.IGNORECASE) # Match smiles
 
 global inchi_pattern
-inchi_pattern = re.compile(r"(InChI=.*|\/[0-9A-Z]*\/)", flags=re.IGNORECASE) # Match inchi
+inchi_pattern = re.compile(r"(InChI=.*\/[0-9A-Z]*\/.*)", flags=re.IGNORECASE) # Match inchi
 
 global inchikey_pattern
 inchikey_pattern = re.compile(r"([A-Z]{14}-[A-Z]{10}-[NO])", flags=re.IGNORECASE) # Match inchikey
@@ -99,6 +99,9 @@ def repair_mol_descriptors(metadata_dict):
     if not inchi_in_inchi:
         metadata_dict['INCHI'] = ''
 
+    # Again call the repair InChI function to repair and update the InChI
+    metadata_dict = repair_inchi(metadata_dict)
+
     # SMILES
     smiles_in_smiles = re.search(smiles_pattern, smiles)
     smiles_in_inchi = re.search(smiles_pattern, inchi)
@@ -106,25 +109,32 @@ def repair_mol_descriptors(metadata_dict):
     smiles_in_name = re.search(smiles_pattern, name)
     smiles_in_comment = re.search(smiles_pattern, comment)
 
+    inchi_in_inchi = re.search(inchi_pattern, inchi)
+    inchikey_in_inchikey = re.search(inchikey_pattern, inchikey)
+
     if smiles_in_smiles:
-        metadata_dict['SMILES'] = smiles_in_smiles.group(1)
+        if any(c in smiles_in_smiles.group(1) for c in ["C", "c"]) and "(" in smiles_in_smiles.group(1) and ")" in smiles_in_smiles.group(1):
+            metadata_dict['SMILES'] = smiles_in_smiles.group(1)
     if smiles_in_inchi:
-        metadata_dict['SMILES'] = smiles_in_inchi.group(1)
-        metadata_dict['INCHI'] = inchi.replace(smiles_in_inchi.group(1), '')
+        if not inchi_in_inchi:
+            if any(c in smiles_in_inchi.group(1) for c in ["C", "c"]) and "(" in smiles_in_inchi.group(1) and ")" in smiles_in_inchi.group(1):
+                metadata_dict['SMILES'] = smiles_in_inchi.group(1)
+                metadata_dict['INCHI'] = inchi.replace(smiles_in_inchi.group(1), '')
     if smiles_in_inchikey:
-        metadata_dict['SMILES'] = smiles_in_inchikey.group(1)
-        metadata_dict['INCHIKEY'] = inchikey.replace(smiles_in_inchikey.group(1), '')
+        if not inchikey_in_inchikey:
+            if any(c in smiles_in_inchikey.group(1) for c in ["C", "c"]) and "(" in smiles_in_inchikey.group(1) and ")" in smiles_in_inchikey.group(1):
+                metadata_dict['SMILES'] = smiles_in_inchikey.group(1)
+                metadata_dict['INCHIKEY'] = inchikey.replace(smiles_in_inchikey.group(1), '')
     if smiles_in_name:
-        metadata_dict['SMILES'] = smiles_in_name.group(1)
-        metadata_dict['NAME'] = name.replace(smiles_in_name.group(1), '')
+        if any(c in smiles_in_name.group(1) for c in ["C", "c"]) and "(" in smiles_in_name.group(1) and ")" in smiles_in_name.group(1):
+            metadata_dict['SMILES'] = smiles_in_name.group(1)
+            metadata_dict['NAME'] = name.replace(smiles_in_name.group(1), '')
     if smiles_in_comment:
-        metadata_dict['SMILES'] = smiles_in_comment.group(1)
-        metadata_dict['COMMENT'] = comment.replace(smiles_in_comment.group(1), '')
+        if any(c in smiles_in_comment.group(1) for c in ["C", "c"]) and "(" in smiles_in_comment.group(1) and ")" in smiles_in_comment.group(1):
+            metadata_dict['SMILES'] = smiles_in_comment.group(1)
+            metadata_dict['COMMENT'] = comment.replace(smiles_in_comment.group(1), '')
     if not smiles_in_smiles:
         metadata_dict['SMILES'] = ''
-
-    # Again call the repair InChI function to repair and update the InChI
-    metadata_dict = repair_inchi(metadata_dict)
 
     # Return the updated metadata dictionary
     return metadata_dict
