25da011c5934ba4a422808b4b299167924b6b1cc
Axel.Dablanc <axel.dablanc@univ-tlse3.fr>
2024-12-17 10:52:27 +0100

    Add callback support for progress monitoring in csv_to_msp
    Enhances the `dataframe_to_msp` and `csv_to_msp` functions to support multiple callback options for progress tracking, such as progress updates, total item notification, and task prefix/context changes. This improves usability when processing large datasets by enabling real-time updates during data conversion.

--- scripts/MAIN.py
+++ scripts/MAIN.py
@@ -171,7 +171,7 @@ def MAIN(progress_callback=None, total_items_callback=None, prefix_callback=None
         time.sleep(0.01)
         print("{:>70}".format("-- CONVERTING CSV TO MSP --"))
         time.sleep(0.01)
-        POS_LC_df, POS_LC, POS_LC_df_insilico, POS_LC_insilico, POS_GC_df, POS_GC, POS_GC_df_insilico, POS_GC_insilico, NEG_LC_df, NEG_LC, NEG_LC_df_insilico, NEG_LC_insilico, NEG_GC_df, NEG_GC, NEG_GC_df_insilico, NEG_GC_insilico = csv_to_msp(POS_LC_df, POS_LC_In_Silico_df, POS_GC_df, POS_GC_In_Silico_df, NEG_LC_df, NEG_LC_In_Silico_df, NEG_GC_df, NEG_GC_In_Silico_df)
+        POS_LC_df, POS_LC, POS_LC_df_insilico, POS_LC_insilico, POS_GC_df, POS_GC, POS_GC_df_insilico, POS_GC_insilico, NEG_LC_df, NEG_LC, NEG_LC_df_insilico, NEG_LC_insilico, NEG_GC_df, NEG_GC, NEG_GC_df_insilico, NEG_GC_insilico = csv_to_msp(POS_LC_df, POS_LC_In_Silico_df, POS_GC_df, POS_GC_In_Silico_df, NEG_LC_df, NEG_LC_In_Silico_df, NEG_GC_df, NEG_GC_In_Silico_df, progress_callback=progress_callback, total_items_callback=total_items_callback, prefix_callback=prefix_callback, item_type_callback=item_type_callback)
 
     # STEP 9: writting output files
     if parameters_dict["csv"] == 1.0:
--- scripts/convertors/csv_to_msp.py
+++ scripts/convertors/csv_to_msp.py
@@ -17,69 +17,77 @@ def format_comments(DF_row):
     # If it exists, include the attribute value. If not, include 'UNKNOWN'.
     return f'FILENAME={DF_row["FILENAME"] if DF_row["FILENAME"] else "UNKNOWN"}; PREDICTED={DF_row["PREDICTED"] if DF_row["PREDICTED"] else "UNKNOWN"}; SPLASH={DF_row["SPLASH"] if DF_row["SPLASH"] else "UNKNOWN"}; SPECTRUMID={DF_row["SPECTRUMID"] if DF_row["SPECTRUMID"] else "UNKNOWN"}; RESOLUTION={DF_row["RESOLUTION"] if DF_row["RESOLUTION"] else "UNKNOWN"}; SYNON={DF_row["SYNON"] if DF_row["SYNON"] else "UNKNOWN"}; FRAGMENTATIONMODE={DF_row["FRAGMENTATIONMODE"] if DF_row["FRAGMENTATIONMODE"] else "UNKNOWN"}; AVERAGEMASS={DF_row["AVERAGEMASS"] if DF_row["AVERAGEMASS"] else "UNKNOWN"}; ENTROPY={DF_row["ENTROPY"] if DF_row["ENTROPY"] else "UNKNOWN"}; ONTOLOGIES = "CLASSYFIRE_SUPERCLASS={DF_row['CLASSYFIRE_SUPERCLASS'] if DF_row['CLASSYFIRE_SUPERCLASS'] else 'UNKNOWN'}, CLASSYFIRE_CLASS = {DF_row['CLASSYFIRE_CLASS'] if DF_row['CLASSYFIRE_CLASS'] else 'UNKNOWN'}, CLASSYFIRE_SUBCLASS = {DF_row['CLASSYFIRE_SUBCLASS'] if DF_row['CLASSYFIRE_SUBCLASS'] else 'UNKNOWN'}, NPCLASS_PATHWAY = {DF_row['NPCLASS_PATHWAY'] if DF_row['NPCLASS_PATHWAY'] else 'UNKNOWN'}, NPCLASS_SUPERCLASS = {DF_row['NPCLASS_SUPERCLASS'] if DF_row['NPCLASS_SUPERCLASS'] else 'UNKNOWN'}, NPCLASS_CLASS = {DF_row['NPCLASS_CLASS'] if DF_row['NPCLASS_CLASS'] else 'UNKNOWN'}"'
 
-def dataframe_to_msp(dataframe, name):
+def dataframe_to_msp(dataframe, name, progress_callback=None, total_items_callback=None, prefix_callback=None,
+                     item_type_callback=None):
     """
-    :param dataframe: A pandas DataFrame containing spectral data.
-    :param name: The name of the spectrum. Used for tqdm progress bar description.
-    :return: A list of formatted spectra strings.
+    Convertit un DataFrame contenant des données spectrales en une liste de chaînes formatées représentant les spectres.
 
-    This method takes a DataFrame containing spectral data and converts it into a list of formatted spectra strings. Each row in the DataFrame represents a spectrum, and the columns contain
-    * different attributes of the spectrum such as name, precursor m/z, precursor type, etc.
-
-    The method iterates over each row in the DataFrame and formats the spectrum information into a string using a predefined format. The resulting formatted spectrum strings are appended
-    * to the spectrum_list.
-
-    The method uses the tqdm library to display a progress bar while iterating over the rows of the DataFrame. The name parameter is used as the description for the progress bar.
-
-    Example usage:
-        dataframe = pd.DataFrame(...)  # Create a DataFrame with spectral data
-        name = "Spectrum Conversion"  # Specify a name for the spectrum
-        spectra = dataframe_to_msp(dataframe, name)  # Convert DataFrame to list of formatted spectra strings
+    :param dataframe: Un DataFrame pandas contenant les données spectrales.
+    :param name: Le nom du spectre (sert de contexte pour les callbacks).
+    :param progress_callback: Fonction callable pour notifier la progression.
+    :param total_items_callback: Fonction callable pour définir le total des éléments.
+    :param prefix_callback: Fonction callable pour signaler le contexte de la tâche.
+    :param item_type_callback: Fonction callable pour signaler le type des éléments traités.
+    :return: Une liste de chaînes formatées représentant les spectres.
     """
-    # Convert the data type of all columns in the dataframe to string
+
+    # Convertir toutes les colonnes en type chaîne
     dataframe = dataframe.astype(str)
 
-    # Initialize an empty list to store the formatted spectra strings
+    # Indique la tâche en cours
+    if prefix_callback:
+        prefix_callback(f"Formatting {name} to MSP")
+
+    # Définir le type d'éléments traités
+    if item_type_callback:
+        item_type_callback("rows")
+
+    # Signaler le total des éléments à traiter
+    total_rows = len(dataframe)
+    if total_items_callback:
+        total_items_callback(total_rows, 0)  # Initialisation des éléments complétés
+
+    # Initialiser une liste pour les chaînes formatées
     spectrum_list = []
 
-    # Iterate over each row in the dataframe
-    with tqdm(total=len(dataframe), desc="{:>70}".format(name), colour="green", unit=" row") as pbar:
-        for index, row in dataframe.iterrows():
-            # Format the comments for the current row
-            COMMENTS = format_comments(row)
+    # Parcourir chaque ligne du DataFrame
+    for index, row in dataframe.iterrows():
+        # Formater le commentaire pour la ligne courante
+        COMMENTS = format_comments(row)
 
-            # Here begins the creation of the spectrum string based on the values in the row
-            # Each attribute is checked if it exists in the row; if it doesn't, the value "UNKNOWN" is used
-            # The following block does this for several attributes such as NAME, PRECURSORMZ, PRECURSORTYPE, etc.
-            SPECTRUM = ""
-            SPECTRUM = SPECTRUM + "NAME: " + (row["NAME"] if row["NAME"] else "UNKNOWN") + "\n"
-            SPECTRUM = SPECTRUM + "PRECURSORMZ: " + (row["PRECURSORMZ"] if row["PRECURSORMZ"] else "UNKNOWN") + "\n"
-            SPECTRUM = SPECTRUM + "PRECURSORTYPE: " + (row["PRECURSORTYPE"] if row["PRECURSORTYPE"] else "UNKNOWN") + "\n"
-            SPECTRUM = SPECTRUM + "FORMULA: " + (row["FORMULA"] if row["FORMULA"] else "UNKNOWN") + "\n"
-            # Ontology ???
-            SPECTRUM = SPECTRUM + "INCHIKEY: " + (row["INCHIKEY"] if row["INCHIKEY"] else "UNKNOWN") + "\n"
-            SPECTRUM = SPECTRUM + "INCHI: " + (row["INCHI"] if row["INCHI"] else "UNKNOWN") + "\n"
-            SPECTRUM = SPECTRUM + "SMILES: " + (row["SMILES"] if row["SMILES"] else "UNKNOWN") + "\n"
-            SPECTRUM = SPECTRUM + "RETENTIONTIME: " + (row["RETENTIONTIME"] if row["RETENTIONTIME"] else "UNKNOWN") + "\n"
-            SPECTRUM = SPECTRUM + "IONMODE: " + (row["IONMODE"] if row["IONMODE"] else "UNKNOWN") + "\n"
-            SPECTRUM = SPECTRUM + "INSTRUMENTTYPE: " + (row["INSTRUMENTTYPE"] if row["INSTRUMENTTYPE"] else "UNKNOWN") + "\n"
-            SPECTRUM = SPECTRUM + "INSTRUMENT: " + (row["INSTRUMENT"] if row["INSTRUMENT"] else "UNKNOWN") + "\n"
-            SPECTRUM = SPECTRUM + "COLLISIONENERGY: " + (row["COLLISIONENERGY"] if row["COLLISIONENERGY"] else "UNKNOWN") + "\n"
-            SPECTRUM = SPECTRUM + "EXACTMASS: " + (str(row["EXACTMASS"]) if row["EXACTMASS"] else "UNKNOWN") + "\n"
-            SPECTRUM = SPECTRUM + "IONIZATION: " + (row["IONIZATION"] if row["IONIZATION"] else "UNKNOWN") + "\n"
-            SPECTRUM = SPECTRUM + "MSLEVEL: " + (str(row["MSLEVEL"]) if row["MSLEVEL"] else "UNKNOWN") + "\n"
-            SPECTRUM = SPECTRUM + "COMMENT: " + (COMMENTS if COMMENTS else "UNKNOWN") + "\n"
-            SPECTRUM = SPECTRUM + "NUM PEAKS: " + (row["NUM PEAKS"] if row["NUM PEAKS"] else "UNKNOWN") + "\n"
-            SPECTRUM = SPECTRUM + row["PEAKS_LIST"] + "\n"
-            # Finally, append the formatted spectrum string for the current row to the spectrum_list
-            spectrum_list.append(SPECTRUM)
-            pbar.update()
-        pbar.close()
+        # Créer la chaîne du spectre basé sur les valeurs de la ligne
+        SPECTRUM = ""
+        SPECTRUM += "NAME: " + (row["NAME"] if row["NAME"] else "UNKNOWN") + "\n"
+        SPECTRUM += "PRECURSORMZ: " + (row["PRECURSORMZ"] if row["PRECURSORMZ"] else "UNKNOWN") + "\n"
+        SPECTRUM += "PRECURSORTYPE: " + (row["PRECURSORTYPE"] if row["PRECURSORTYPE"] else "UNKNOWN") + "\n"
+        SPECTRUM += "FORMULA: " + (row["FORMULA"] if row["FORMULA"] else "UNKNOWN") + "\n"
+        SPECTRUM += "INCHIKEY: " + (row["INCHIKEY"] if row["INCHIKEY"] else "UNKNOWN") + "\n"
+        SPECTRUM += "INCHI: " + (row["INCHI"] if row["INCHI"] else "UNKNOWN") + "\n"
+        SPECTRUM += "SMILES: " + (row["SMILES"] if row["SMILES"] else "UNKNOWN") + "\n"
+        SPECTRUM += "RETENTIONTIME: " + (row["RETENTIONTIME"] if row["RETENTIONTIME"] else "UNKNOWN") + "\n"
+        SPECTRUM += "IONMODE: " + (row["IONMODE"] if row["IONMODE"] else "UNKNOWN") + "\n"
+        SPECTRUM += "INSTRUMENTTYPE: " + (row["INSTRUMENTTYPE"] if row["INSTRUMENTTYPE"] else "UNKNOWN") + "\n"
+        SPECTRUM += "INSTRUMENT: " + (row["INSTRUMENT"] if row["INSTRUMENT"] else "UNKNOWN") + "\n"
+        SPECTRUM += "COLLISIONENERGY: " + (row["COLLISIONENERGY"] if row["COLLISIONENERGY"] else "UNKNOWN") + "\n"
+        SPECTRUM += "EXACTMASS: " + (row["EXACTMASS"] if row["EXACTMASS"] else "UNKNOWN") + "\n"
+        SPECTRUM += "IONIZATION: " + (row["IONIZATION"] if row["IONIZATION"] else "UNKNOWN") + "\n"
+        SPECTRUM += "MSLEVEL: " + (row["MSLEVEL"] if row["MSLEVEL"] else "UNKNOWN") + "\n"
+        SPECTRUM += "COMMENT: " + (COMMENTS if COMMENTS else "UNKNOWN") + "\n"
+        SPECTRUM += "NUM PEAKS: " + (row["NUM PEAKS"] if row["NUM PEAKS"] else "UNKNOWN") + "\n"
+        SPECTRUM += row["PEAKS_LIST"] + "\n"
 
-    # Return the list of formatted spectra strings
+        # Ajouter le spectre formaté à la liste des spectres
+        spectrum_list.append(SPECTRUM)
+
+        # Mise à jour de la progression
+        if progress_callback:
+            progress_callback(index + 1)
+
+    # Retourner la liste des spectres formatés
     return spectrum_list
 
-def csv_to_msp(POS_LC_df,POS_LC_df_insilico,POS_GC_df,POS_GC_df_insilico,NEG_LC_df,NEG_LC_df_insilico,NEG_GC_df,NEG_GC_df_insilico):
+
+def csv_to_msp(POS_LC_df,POS_LC_df_insilico,POS_GC_df,POS_GC_df_insilico,NEG_LC_df,NEG_LC_df_insilico,NEG_GC_df,NEG_GC_df_insilico, progress_callback=None, total_items_callback=None, prefix_callback=None, item_type_callback=None):
     """
     :param POS_LC_df: DataFrame containing positive LC data
     :param POS_LC_df_insilico: DataFrame containing positive LC insilico data
@@ -109,21 +117,21 @@ def csv_to_msp(POS_LC_df,POS_LC_df_insilico,POS_GC_df,POS_GC_df_insilico,NEG_LC_
 
     """
     # Create MSP file from POS_LC dataframe
-    POS_LC = dataframe_to_msp(POS_LC_df, "POS_LC")
+    POS_LC = dataframe_to_msp(POS_LC_df, "POS_LC", progress_callback=progress_callback, total_items_callback=total_items_callback, prefix_callback=prefix_callback, item_type_callback=item_type_callback)
     # Create MSP file from POS_LC insilico dataframe
-    POS_LC_insilico = dataframe_to_msp(POS_LC_df_insilico, "POS_LC_insilico")
+    POS_LC_insilico = dataframe_to_msp(POS_LC_df_insilico, "POS_LC_insilico", progress_callback=progress_callback, total_items_callback=total_items_callback, prefix_callback=prefix_callback, item_type_callback=item_type_callback)
     # Create MSP file from POS_GC dataframe
     POS_GC = dataframe_to_msp(POS_GC_df, "POS_GC")
     # Create MSP file from POS_GC insilico dataframe
-    POS_GC_insilico = dataframe_to_msp(POS_GC_df_insilico, "POS_GC_insilico")
+    POS_GC_insilico = dataframe_to_msp(POS_GC_df_insilico, "POS_GC_insilico", progress_callback=progress_callback, total_items_callback=total_items_callback, prefix_callback=prefix_callback, item_type_callback=item_type_callback)
     # Create MSP file from NEG_LC dataframe
     NEG_LC = dataframe_to_msp(NEG_LC_df, "NEG_LC")
     # Create MSP file from NEG_LC insilico dataframe
-    NEG_LC_insilico = dataframe_to_msp(NEG_LC_df_insilico, "NEG_LC_insilico")
+    NEG_LC_insilico = dataframe_to_msp(NEG_LC_df_insilico, "NEG_LC_insilico", progress_callback=progress_callback, total_items_callback=total_items_callback, prefix_callback=prefix_callback, item_type_callback=item_type_callback)
     # Create MSP file from NEG_GC dataframe
     NEG_GC = dataframe_to_msp(NEG_GC_df, "NEG_GC")
     # Create MSP file from NEG_GC insilico dataframe
-    NEG_GC_insilico = dataframe_to_msp(NEG_GC_df_insilico, "NEG_GC_insilico")
+    NEG_GC_insilico = dataframe_to_msp(NEG_GC_df_insilico, "NEG_GC_insilico", progress_callback=progress_callback, total_items_callback=total_items_callback, prefix_callback=prefix_callback, item_type_callback=item_type_callback)
 
     # Return original dataframes as well as the generated MSP files.
     return POS_LC_df, POS_LC, POS_LC_df_insilico, POS_LC_insilico, POS_GC_df, POS_GC, POS_GC_df_insilico, POS_GC_insilico, NEG_LC_df, NEG_LC, NEG_LC_df_insilico, NEG_LC_insilico, NEG_GC_df, NEG_GC, NEG_GC_df_insilico, NEG_GC_insilico
