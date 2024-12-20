ba7fea34aae251e0ee86a131ccfdfeb511592714
Axel.Dablanc <axel.dablanc@univ-tlse3.fr>
2024-12-20 14:51:57 +0100

    Refactor key conversion logic to use global variables.
    Moved keys_dict and keys_list initialization to globals_vars.py for better modularity and reusability. Updated keys_convertor.py to reference the global variables, simplifying the script and reducing duplication.

--- scripts/convertors/keys_convertor.py
+++ scripts/convertors/keys_convertor.py
@@ -1,44 +1,7 @@
 import pandas as pd
+import globals_vars
 import os
 
-global keys_dict
-Key_dataframe = pd.read_csv(os.path.abspath("../datas/key_to_convert.csv"),sep=";", encoding="UTF-8") # Remplacez 'your_file.csv' par le chemin de votre fichier
-keys_dict = dict(zip(Key_dataframe['known_synonym'], Key_dataframe['fraghub_default'].str.upper()))
-
-global keys_list
-keys_list = ['FILENAME',
-             'PREDICTED',
-             'SPLASH',
-             'SPECTRUMID',
-             'RESOLUTION',
-             'SYNON',
-             'IONIZATION',
-             'MSLEVEL',
-             'FRAGMENTATIONMODE',
-             'NAME',
-             'PRECURSORMZ',
-             'EXACTMASS',
-             'AVERAGEMASS',
-             'PRECURSORTYPE',
-             'INSTRUMENTTYPE',
-             'INSTRUMENT',
-             'SMILES',
-             'INCHI',
-             'INCHIKEY',
-             'COLLISIONENERGY',
-             'FORMULA',
-             'RETENTIONTIME',
-             'IONMODE',
-             'COMMENT',
-             'ENTROPY',
-             'CLASSYFIRE_SUPERCLASS',
-             'CLASSYFIRE_CLASS',
-             'CLASSYFIRE_SUBCLASS',
-             'NPCLASS_PATHWAY',
-             'NPCLASS_SUPERCLASS',
-             'NPCLASS_CLASS',
-             'NUM PEAKS',
-             'PEAKS_LIST']
 
 def convert_keys(metadata_dict):
     """
@@ -52,12 +15,12 @@ def convert_keys(metadata_dict):
     # Creating a dictionary comprehension that converts all keys from metadata to lower case
     # and matches them with the keys available in keys_dict and keys_list.
     # The output is a dictionary where the keys are mapped from keys_dict and the values come from metadata_dict.
-    converted = {keys_dict[key.lower()]: val for key, val in metadata_dict.items() if key.lower() in keys_dict and keys_dict[key.lower()] in keys_list}
+    converted = {globals_vars.keys_dict[key.lower()]: val for key, val in metadata_dict.items() if key.lower() in keys_dict and keys_dict[key.lower()] in keys_list}
     del metadata_dict
 
     # After initial conversion, there might still be some keys from keys_list that are not in the converted dictionary.
     # This line adds those missing keys to the converted dictionary with an empty string ("") as their value.
-    converted.update({key: "" for key in keys_list if key not in converted})
+    converted.update({key: "" for key in globals_vars.keys_list if key not in converted})
 
     # Returning the result that is a dictionary with matching and new keys.
     return converted
--- scripts/globals_vars.py
+++ scripts/globals_vars.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import psutil
 import os
 import re
@@ -14,3 +15,42 @@ peak_list_csv_to_json_pattern = re.compile(r"(-?\d+\.?\d*(?:[Ee][+-]?\d+)?)(?:\s
 
 global peak_list_json_to_json_pattern
 peak_list_json_to_json_pattern = re.compile(r"(-?\d+\.?\d*(?:[Ee][+-]?\d+)?)(?:\s+|:|,|, )(-?\d+[.,]?\d*(?:[Ee][+-]?\d+)?)")
+
+global keys_dict
+Key_dataframe = pd.read_csv(os.path.abspath("../datas/key_to_convert.csv"),sep=";", encoding="UTF-8") # Remplacez 'your_file.csv' par le chemin de votre fichier
+keys_dict = dict(zip(Key_dataframe['known_synonym'], Key_dataframe['fraghub_default'].str.upper()))
+
+global keys_list
+keys_list = ['FILENAME',
+             'PREDICTED',
+             'SPLASH',
+             'SPECTRUMID',
+             'RESOLUTION',
+             'SYNON',
+             'IONIZATION',
+             'MSLEVEL',
+             'FRAGMENTATIONMODE',
+             'NAME',
+             'PRECURSORMZ',
+             'EXACTMASS',
+             'AVERAGEMASS',
+             'PRECURSORTYPE',
+             'INSTRUMENTTYPE',
+             'INSTRUMENT',
+             'SMILES',
+             'INCHI',
+             'INCHIKEY',
+             'COLLISIONENERGY',
+             'FORMULA',
+             'RETENTIONTIME',
+             'IONMODE',
+             'COMMENT',
+             'ENTROPY',
+             'CLASSYFIRE_SUPERCLASS',
+             'CLASSYFIRE_CLASS',
+             'CLASSYFIRE_SUBCLASS',
+             'NPCLASS_PATHWAY',
+             'NPCLASS_SUPERCLASS',
+             'NPCLASS_CLASS',
+             'NUM PEAKS',
+             'PEAKS_LIST']
