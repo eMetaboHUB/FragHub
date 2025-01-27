8ce1c9694dfb175c7a4ac20553141482ce60e80c
Axel.Dablanc <axel.dablanc@univ-tlse3.fr>
2025-01-27 14:06:34 +0100

    Add global report dictionary to track spectrum statistics
    Introduced `report_dict` in `global_report.py` to store statistics related to spectrum counts and unique InChIKeys. This serves as a centralized structure for tracking both experimental and in-silico data for LC and GC methods in positive and negative modes.

---
+++ scripts/global_report.py
@@ -0,0 +1,20 @@
+
+global report_dict
+report_dict = {"pos_lc_exp_spectrum_number": 0,
+               "neg_lc_exp_spectrum_number": 0,
+               "pos_lc_insilico_spectrum_number": 0,
+               "neg_lc_insilico_spectrum_number": 0,
+               "pos_gc_exp_spectrum_number": 0,
+               "neg_gc_exp_spectrum_number": 0,
+               "pos_gc_insilico_spectrum_number": 0,
+               "neg_gc_insilico_spectrum_number": 0,
+               "pos_lc_exp_spectrum_unique_inchikey": 0,
+               "neg_lc_exp_spectrum_unique_inchikey": 0,
+               "pos_lc_insilico_spectrum_unique_inchikey": 0,
+               "neg_lc_insilico_spectrum_unique_inchikey": 0,
+               "pos_gc_exp_spectrum_unique_inchikey": 0,
+               "neg_gc_exp_spectrum_unique_inchikey": 0,
+               "pos_gc_insilico_spectrum_unique_inchikey": 0,
+               "neg_gc_insilico_spectrum_unique_inchikey": 0,
+               "TOTAL_unique_inchikey": 0
+               }
