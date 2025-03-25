b65a019dab3632c4045221752ca93263787cd59b
Axel.Dablanc <axel.dablanc@univ-tlse3.fr>
2025-03-25 14:30:04 +0100

    Update version number to 1.2.5 in window titles
    Updated the displayed version number from 1.2.4 to 1.2.5 in both `FragHub.py` and `progress_window.py` to reflect the latest version. This ensures consistency in the application's branding and versioning.

--- scripts/progress_window.py
+++ scripts/progress_window.py
@@ -144,7 +144,7 @@ class ProgressWindow(QMainWindow):
         super().__init__(parent)
 
         # Titre et icône de la fenêtre
-        self.setWindowTitle("FragHub 1.2.4")
+        self.setWindowTitle("FragHub 1.2.5")
         self.setWindowIcon(QIcon("./GUI/assets/FragHub_icon.png"))
         self.setGeometry(100, 100, 1280, 720)
 
