10124e4cb1248367d56c17f2ec51b9f5e1748d30
Axel.Dablanc <axel.dablanc@univ-tlse3.fr>
2025-09-01 10:15:11 +0200

    Update default missing data placeholder in `csv_to_msp.py`
    Replaced "UNKNOWN" with "NOT FOUND" as the default placeholder for missing data in spectrum generation, ensuring clarity and consistency in output formatting.

--- scripts/convertors/csv_to_msp.py
+++ scripts/convertors/csv_to_msp.py
@@ -14,8 +14,8 @@ def format_comments(DF_row):
 
     # Use a formatted string (f-string) to create the output string.
     # For each attribute, use a ternary conditional operator to check if the attribute value exists in the row.
-    # If it exists, include the attribute value. If not, include 'UNKNOWN'.
-    return f'FILENAME={DF_row["FILENAME"] if DF_row["FILENAME"] else "UNKNOWN"}; PREDICTED={DF_row["PREDICTED"] if DF_row["PREDICTED"] else "UNKNOWN"}; SPLASH={DF_row["SPLASH"] if DF_row["SPLASH"] else "UNKNOWN"}; SPECTRUMID={DF_row["SPECTRUMID"] if DF_row["SPECTRUMID"] else "UNKNOWN"}; RESOLUTION={DF_row["RESOLUTION"] if DF_row["RESOLUTION"] else "UNKNOWN"}; SYNON={DF_row["SYNON"] if DF_row["SYNON"] else "UNKNOWN"}; FRAGMENTATIONMODE={DF_row["FRAGMENTATIONMODE"] if DF_row["FRAGMENTATIONMODE"] else "UNKNOWN"}; AVERAGEMASS={DF_row["AVERAGEMASS"] if DF_row["AVERAGEMASS"] else "UNKNOWN"}; ENTROPY={DF_row["ENTROPY"] if DF_row["ENTROPY"] else "UNKNOWN"}; ONTOLOGIES = "CLASSYFIRE_SUPERCLASS={DF_row['CLASSYFIRE_SUPERCLASS'] if DF_row['CLASSYFIRE_SUPERCLASS'] else 'UNKNOWN'}, CLASSYFIRE_CLASS = {DF_row['CLASSYFIRE_CLASS'] if DF_row['CLASSYFIRE_CLASS'] else 'UNKNOWN'}, CLASSYFIRE_SUBCLASS = {DF_row['CLASSYFIRE_SUBCLASS'] if DF_row['CLASSYFIRE_SUBCLASS'] else 'UNKNOWN'}, NPCLASS_PATHWAY = {DF_row['NPCLASS_PATHWAY'] if DF_row['NPCLASS_PATHWAY'] else 'UNKNOWN'}, NPCLASS_SUPERCLASS = {DF_row['NPCLASS_SUPERCLASS'] if DF_row['NPCLASS_SUPERCLASS'] else 'UNKNOWN'}, NPCLASS_CLASS = {DF_row['NPCLASS_CLASS'] if DF_row['NPCLASS_CLASS'] else 'UNKNOWN'}"'
+    # If it exists, include the attribute value. If not, include 'NOT FOUND'.
+    return f'FILENAME={DF_row["FILENAME"] if DF_row["FILENAME"] else "NOT FOUND"}; PREDICTED={DF_row["PREDICTED"] if DF_row["PREDICTED"] else "NOT FOUND"}; SPLASH={DF_row["SPLASH"] if DF_row["SPLASH"] else "NOT FOUND"}; SPECTRUMID={DF_row["SPECTRUMID"] if DF_row["SPECTRUMID"] else "NOT FOUND"}; RESOLUTION={DF_row["RESOLUTION"] if DF_row["RESOLUTION"] else "NOT FOUND"}; SYNON={DF_row["SYNON"] if DF_row["SYNON"] else "NOT FOUND"}; FRAGMENTATIONMODE={DF_row["FRAGMENTATIONMODE"] if DF_row["FRAGMENTATIONMODE"] else "NOT FOUND"}; AVERAGEMASS={DF_row["AVERAGEMASS"] if DF_row["AVERAGEMASS"] else "NOT FOUND"}; ENTROPY={DF_row["ENTROPY"] if DF_row["ENTROPY"] else "NOT FOUND"}; ONTOLOGIES = "CLASSYFIRE_SUPERCLASS={DF_row['CLASSYFIRE_SUPERCLASS'] if DF_row['CLASSYFIRE_SUPERCLASS'] else 'NOT FOUND'}, CLASSYFIRE_CLASS = {DF_row['CLASSYFIRE_CLASS'] if DF_row['CLASSYFIRE_CLASS'] else 'NOT FOUND'}, CLASSYFIRE_SUBCLASS = {DF_row['CLASSYFIRE_SUBCLASS'] if DF_row['CLASSYFIRE_SUBCLASS'] else 'NOT FOUND'}, NPCLASS_PATHWAY = {DF_row['NPCLASS_PATHWAY'] if DF_row['NPCLASS_PATHWAY'] else 'NOT FOUND'}, NPCLASS_SUPERCLASS = {DF_row['NPCLASS_SUPERCLASS'] if DF_row['NPCLASS_SUPERCLASS'] else 'NOT FOUND'}, NPCLASS_CLASS = {DF_row['NPCLASS_CLASS'] if DF_row['NPCLASS_CLASS'] else 'NOT FOUND'}"'
 
 def dataframe_to_msp(dataframe, name, progress_callback=None, total_items_callback=None, prefix_callback=None, item_type_callback=None):
     """
@@ -58,23 +58,23 @@ def dataframe_to_msp(dataframe, name, progress_callback=None, total_items_callba
 
         # Créer la chaîne du spectre basé sur les valeurs de la ligne
         SPECTRUM = ""
-        SPECTRUM += "NAME: " + (row["NAME"] if row["NAME"] else "UNKNOWN") + "\n"
-        SPECTRUM += "PRECURSORMZ: " + (row["PRECURSORMZ"] if row["PRECURSORMZ"] else "UNKNOWN") + "\n"
-        SPECTRUM += "PRECURSORTYPE: " + (row["PRECURSORTYPE"] if row["PRECURSORTYPE"] else "UNKNOWN") + "\n"
-        SPECTRUM += "FORMULA: " + (row["FORMULA"] if row["FORMULA"] else "UNKNOWN") + "\n"
-        SPECTRUM += "INCHIKEY: " + (row["INCHIKEY"] if row["INCHIKEY"] else "UNKNOWN") + "\n"
-        SPECTRUM += "INCHI: " + (row["INCHI"] if row["INCHI"] else "UNKNOWN") + "\n"
-        SPECTRUM += "SMILES: " + (row["SMILES"] if row["SMILES"] else "UNKNOWN") + "\n"
-        SPECTRUM += "RT: " + (row["RT"] if row["RT"] else "UNKNOWN") + "\n"
-        SPECTRUM += "IONMODE: " + (row["IONMODE"] if row["IONMODE"] else "UNKNOWN") + "\n"
-        SPECTRUM += "INSTRUMENTTYPE: " + (row["INSTRUMENTTYPE"] if row["INSTRUMENTTYPE"] else "UNKNOWN") + "\n"
-        SPECTRUM += "INSTRUMENT: " + (row["INSTRUMENT"] if row["INSTRUMENT"] else "UNKNOWN") + "\n"
-        SPECTRUM += "COLLISIONENERGY: " + (row["COLLISIONENERGY"] if row["COLLISIONENERGY"] else "UNKNOWN") + "\n"
-        SPECTRUM += "EXACTMASS: " + (row["EXACTMASS"] if row["EXACTMASS"] else "UNKNOWN") + "\n"
-        SPECTRUM += "IONIZATION: " + (row["IONIZATION"] if row["IONIZATION"] else "UNKNOWN") + "\n"
-        SPECTRUM += "MSLEVEL: " + (row["MSLEVEL"] if row["MSLEVEL"] else "UNKNOWN") + "\n"
-        SPECTRUM += "COMMENT: " + (COMMENTS if COMMENTS else "UNKNOWN") + "\n"
-        SPECTRUM += "NUM PEAKS: " + (row["NUM PEAKS"] if row["NUM PEAKS"] else "UNKNOWN") + "\n"
+        SPECTRUM += "NAME: " + (row["NAME"] if row["NAME"] else "NOT FOUND") + "\n"
+        SPECTRUM += "PRECURSORMZ: " + (row["PRECURSORMZ"] if row["PRECURSORMZ"] else "NOT FOUND") + "\n"
+        SPECTRUM += "PRECURSORTYPE: " + (row["PRECURSORTYPE"] if row["PRECURSORTYPE"] else "NOT FOUND") + "\n"
+        SPECTRUM += "FORMULA: " + (row["FORMULA"] if row["FORMULA"] else "NOT FOUND") + "\n"
+        SPECTRUM += "INCHIKEY: " + (row["INCHIKEY"] if row["INCHIKEY"] else "NOT FOUND") + "\n"
+        SPECTRUM += "INCHI: " + (row["INCHI"] if row["INCHI"] else "NOT FOUND") + "\n"
+        SPECTRUM += "SMILES: " + (row["SMILES"] if row["SMILES"] else "NOT FOUND") + "\n"
+        SPECTRUM += "RT: " + (row["RT"] if row["RT"] else "NOT FOUND") + "\n"
+        SPECTRUM += "IONMODE: " + (row["IONMODE"] if row["IONMODE"] else "NOT FOUND") + "\n"
+        SPECTRUM += "INSTRUMENTTYPE: " + (row["INSTRUMENTTYPE"] if row["INSTRUMENTTYPE"] else "NOT FOUND") + "\n"
+        SPECTRUM += "INSTRUMENT: " + (row["INSTRUMENT"] if row["INSTRUMENT"] else "NOT FOUND") + "\n"
+        SPECTRUM += "COLLISIONENERGY: " + (row["COLLISIONENERGY"] if row["COLLISIONENERGY"] else "NOT FOUND") + "\n"
+        SPECTRUM += "EXACTMASS: " + (row["EXACTMASS"] if row["EXACTMASS"] else "NOT FOUND") + "\n"
+        SPECTRUM += "IONIZATION: " + (row["IONIZATION"] if row["IONIZATION"] else "NOT FOUND") + "\n"
+        SPECTRUM += "MSLEVEL: " + (row["MSLEVEL"] if row["MSLEVEL"] else "NOT FOUND") + "\n"
+        SPECTRUM += "COMMENT: " + (COMMENTS if COMMENTS else "NOT FOUND") + "\n"
+        SPECTRUM += "NUM PEAKS: " + (row["NUM PEAKS"] if row["NUM PEAKS"] else "NOT FOUND") + "\n"
         SPECTRUM += row["PEAKS_LIST"] + "\n"
 
         # Ajouter le spectre formaté à la liste des spectres
